0c3cd0c1d7e2ff030743cc58ecd869d0efffa64e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-07 20:32:16 +0530

    Advanced DSA & Algorithms: DP: LongestIncreasingSubsequence: completed

--- a_1D_DP_dynamic_programming/LongestIncreasingSubsequence.java
+++ a_1D_DP_dynamic_programming/LongestIncreasingSubsequence.java
@@ -1,5 +1,28 @@
 public class LongestIncreasingSubsequence {
     public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
 
+        for (int num : nums) {
+            int l = 0, r = len;
+
+            while (l < r) {
+                int m = l + (r - l) / 2;
+
+                if (dp[m] < num) {
+                    l = m + 1;
+                } else {
+                    r = m;
+                }
+            }
+
+            dp[l] = num;
+
+            if (l == len) {
+                len++;
+            }
+        }
+
+        return len;
     }
 }
