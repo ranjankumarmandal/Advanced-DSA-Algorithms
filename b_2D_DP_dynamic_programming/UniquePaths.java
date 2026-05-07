713bf8552636f72cbe4ce8d2f584e2c05d7761c1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-07 22:35:33 +0530

    Advanced DSA & Algorithms: 2D DP: UniquePaths: completed

--- b_2D_DP_dynamic_programming/UniquePaths.java
+++ b_2D_DP_dynamic_programming/UniquePaths.java
@@ -1,5 +1,14 @@
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        java.util.Arrays.fill(dp, 1);
 
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[j] += dp[j + 1];
+            }
+        }
+
+        return dp[0];
     }
 }
