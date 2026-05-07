85e176d1d004d400a561f5c534a2a8789551c6b9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-07 22:23:12 +0530

    Advanced DSA & Algorithms: DP: PartitionEqualSubsetSum: completed

--- a_1D_DP_dynamic_programming/PartitionEqualSubsetSum.java
+++ a_1D_DP_dynamic_programming/PartitionEqualSubsetSum.java
@@ -1,5 +1,25 @@
 public class PartitionEqualSubsetSum {
     public boolean canPartition(int[] nums) {
+        int sum = 0;
 
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if ((sum & 1) == 1) {
+            return false;
+        }
+
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
     }
 }
