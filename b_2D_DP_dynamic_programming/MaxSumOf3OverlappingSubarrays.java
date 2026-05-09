6c5556a60ea1d0e595129f366557d7bfe0e031f5
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-09 07:53:01 +0530

    Advanced DSA and Algorithms: 2D DP: MaxSumOf3OverlappingSubarrays: completed

--- b_2D_DP_dynamic_programming/MaxSumOf3OverlappingSubarrays.java
+++ b_2D_DP_dynamic_programming/MaxSumOf3OverlappingSubarrays.java
@@ -1,5 +1,77 @@
 public class MaxSumOf3OverlappingSubarrays {
     public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
 
+        int n = nums.length;
+
+        int[] windowSum = new int[n - k + 1];
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += nums[i];
+
+            if (i >= k) {
+                sum -= nums[i - k];
+            }
+
+            if (i >= k - 1) {
+                windowSum[i - k + 1] = sum;
+            }
+        }
+
+        int m = windowSum.length;
+
+        int[] left = new int[m];
+
+        int best = 0;
+
+        for (int i = 0; i < m; i++) {
+
+            if (windowSum[i] > windowSum[best]) {
+                best = i;
+            }
+
+            left[i] = best;
+        }
+
+        int[] right = new int[m];
+
+        best = m - 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+
+            if (windowSum[i] >= windowSum[best]) {
+                best = i;
+            }
+
+            right[i] = best;
+        }
+
+        int[] ans = new int[3];
+
+        int maxTotal = 0;
+
+        for (int mid = k; mid < m - k; mid++) {
+
+            int l = left[mid - k];
+            int r = right[mid + k];
+
+            int total =
+                    windowSum[l]
+                            + windowSum[mid]
+                            + windowSum[r];
+
+            if (total > maxTotal) {
+
+                maxTotal = total;
+
+                ans[0] = l;
+                ans[1] = mid;
+                ans[2] = r;
+            }
+        }
+
+        return ans;
     }
 }
