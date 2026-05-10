25adfa68719e41333cb407e4654ae52d6598b6b1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-10 12:52:07 +0530

    Advanced DSA and Algorithms: 2D DP: MinimumASCIIDeleteSumForTwoStrings: in-progress v1

--- b_2D_DP_dynamic_programming/MinimumASCIIDeleteSumForTwoStrings.java
+++ b_2D_DP_dynamic_programming/MinimumASCIIDeleteSumForTwoStrings.java
@@ -1,5 +1,17 @@
 public class MinimumASCIIDeleteSumForTwoStrings {
     public int minimumDeleteSum(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (s1.charAt(i) == s2.charAt(j))
+                    dp[i + 1][j + 1] = dp[i][j] + s1.charAt(i);
+                else
+                    dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+
+        return total - 2 * dp[n][m];
     }
 }
