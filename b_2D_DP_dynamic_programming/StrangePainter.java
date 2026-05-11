5e71ad5a7a465e0e3633f63fee8a17467c0f324b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-11 08:24:45 +0530

    Advanced DSA and Algorithms: 2D DP: StrangePainter: in-progress v1

--- b_2D_DP_dynamic_programming/StrangePainter.java
+++ b_2D_DP_dynamic_programming/StrangePainter.java
@@ -1,5 +1,17 @@
 public class StrangePainter {
     public int strangePrinter(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
 
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = dp[i + 1][j] + 1;
+                for (int k = i + 1; k <= j; k++) {
+                }
+            }
+        }
+
+        return dp[0][n - 1];
     }
 }
