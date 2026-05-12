4af97449abfdb2e3bdf6c59d021e23923424b73c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-12 07:35:53 +0530

    Advanced DSA and Algorithms: 2D DP: ChampagneTower: in-progress v1

--- b_2D_DP_dynamic_programming/ChampagneTower.java
+++ b_2D_DP_dynamic_programming/ChampagneTower.java
@@ -1,5 +1,16 @@
 public class ChampagneTower {
     public double champagneTower(int poured, int query_row, int query_glass) {
+        double[][] tower = new double[102][102];
+        tower[0][0] = (double) poured;
 
+        for (int r = 0; r <= query_row; r++) {
+            for (int c = 0; c <= r; c++) {
+                if (tower[r][c] > 1.0) {
+                    double excess = (tower[r][c] - 1.0) / 2.0;
+                    tower[r][c] = 1.0;
+                }
+            }
+        }
+        return tower[query_row][query_glass];
     }
 }
