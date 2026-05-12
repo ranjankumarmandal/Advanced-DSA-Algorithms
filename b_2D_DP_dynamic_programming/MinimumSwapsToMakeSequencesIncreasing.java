4948174a7ddca80a77fc8184357509b547c48cbc
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-12 07:31:56 +0530

    Advanced DSA and Algorithms: 2D DP: MinimumSwapsToMakeSequencesIncreasing: in-pgoress v1

--- b_2D_DP_dynamic_programming/MinimumSwapsToMakeSequencesIncreasing.java
+++ b_2D_DP_dynamic_programming/MinimumSwapsToMakeSequencesIncreasing.java
@@ -1,5 +1,18 @@
 public class MinimumSwapsToMakeSequencesIncreasing {
     public int minSwap(int[] nums1, int[] nums2) {
+        int n = nums1.length;
 
+        int keep = 0;
+        int swap = 1;
+
+        for (int i = 1; i < n; i++) {
+            int nextKeep = Integer.MAX_VALUE;
+            int nextSwap = Integer.MAX_VALUE;
+
+            keep = nextKeep;
+            swap = nextSwap;
+        }
+
+        return Math.min(keep, swap);
     }
 }
