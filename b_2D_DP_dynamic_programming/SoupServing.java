061bfc43c1714a2da68bd0da548ebfad441fc4a7
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-13 07:33:51 +0530

    Advanced DSA and Algorithms: 2D DP: SoupServing: in-progress v1

--- b_2D_DP_dynamic_programming/SoupServing.java
+++ b_2D_DP_dynamic_programming/SoupServing.java
@@ -1,5 +1,25 @@
 public class SoupServing {
-    public double soupServings(int n) {
+    private Double[][] cache;
 
+    public double soupServings(int n) {
+        if (n > 5000) return 1.0;
+
+        int units = (int) Math.ceil(n / 25.0);
+        cache = new Double[units + 1][units + 1];
+
+        return calcProb(units, units);
+    }
+
+    private double calcProb(int soupA, int soupB) {
+        if (soupA <= 0 && soupB <= 0) return 0.5;
+        if (soupA <= 0) return 1.0;
+        if (soupB <= 0) return 0.0;
+
+        if (cache[soupA][soupB] != null) return cache[soupA][soupB];
+
+
+
+        cache[soupA][soupB] = prob;
+        return prob;
     }
 }
