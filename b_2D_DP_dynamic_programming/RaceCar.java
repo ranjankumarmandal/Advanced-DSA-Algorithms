cf3dc8f05dc06e002094dddd18220127a329ceda
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-13 07:27:48 +0530

    Advanced DSA and Algorithms: 2D DP: RaceCar: in-progress v1

--- b_2D_DP_dynamic_programming/RaceCar.java
+++ b_2D_DP_dynamic_programming/RaceCar.java
@@ -1,5 +1,16 @@
 public class RaceCar {
-    public int racecar(int target) {
+    ublic int racecar(int target) {
+        int[] dp = new int[target + 1];
 
+        for (int i = 1; i <= target; i++) {
+            int k = 32 - Integer.numberOfLeadingZeros(i);
+
+            if (i == (1 << k) - 1) {
+                dp[i] = k;
+                continue;
+            }
+        }
+
+        return dp[target];
     }
 }
