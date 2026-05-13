439385598b8d21596451365d8a2fd75fd681322f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-13 07:28:16 +0530

    Advanced DSA and Algorithms: 2D DP: RaceCar: completed

--- b_2D_DP_dynamic_programming/RaceCar.java
+++ b_2D_DP_dynamic_programming/RaceCar.java
@@ -9,6 +9,15 @@ public class RaceCar {
                 dp[i] = k;
                 continue;
             }
+
+            dp[i] = k + 1 + dp[(1 << k) - 1 - i];
+
+            for (int j = 0; j < k - 1; j++) {
+                int cur = (k - 1) + 1 + j + 1
+                        + dp[i - ((1 << (k - 1)) - (1 << j))];
+
+                dp[i] = Math.min(dp[i], cur);
+            }
         }
 
         return dp[target];
