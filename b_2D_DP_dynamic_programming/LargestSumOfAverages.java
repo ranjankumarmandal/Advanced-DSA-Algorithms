1e27b849061d5e631d87e14ea4e81620834b2162
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-13 07:30:57 +0530

    Advanced DSA and Algorithms: 2D DP: LargestSumOfAverages: in-progress v

--- b_2D_DP_dynamic_programming/LargestSumOfAverages.java
+++ b_2D_DP_dynamic_programming/LargestSumOfAverages.java
@@ -1,5 +1,23 @@
 public class LargestSumOfAverages {
     public double largestSumOfAverages(int[] nums, int k) {
+        int n = nums.length;
+        Double[][] dp = new Double[n][k];
+        return divide(nums, 0, n, k-1, dp);
+    }
 
+    public double divide(int[] nums, int start, int end, int k, Double[][] dp) {
+        if(start >= end) return 0;
+        if(dp[start][k] != null) return dp[start][k];
+        if(k == 0) {
+            double lastPartitionSum = 0;
+            for(int j = start; j < end; j++) {
+                lastPartitionSum += nums[j];
+            }
+            return lastPartitionSum / (end - start);
+        }
+
+
+
+        return max;
     }
 }
