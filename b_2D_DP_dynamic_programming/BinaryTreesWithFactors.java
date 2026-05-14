925848b5fbfbbc13aea9170cce9acf006940d944
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-14 07:12:59 +0530

    Advanced DSA and Algorithms: 2D DP: BinaryTreesWithFactors: in-progress v1

--- b_2D_DP_dynamic_programming/BinaryTreesWithFactors.java
+++ b_2D_DP_dynamic_programming/BinaryTreesWithFactors.java
@@ -1,5 +1,26 @@
 public class BinaryTreesWithFactors {
     public int numFactoredBinaryTrees(int[] arr) {
+        int mod = 1_000_000_007;
+        Arrays.sort(arr);
 
+        Map<Integer, Long> dp = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        long result = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            long ways = 1;
+
+
+
+            dp.put(arr[i], ways);
+            result = (result + ways) % mod;
+        }
+
+        return (int) result;
     }
 }
