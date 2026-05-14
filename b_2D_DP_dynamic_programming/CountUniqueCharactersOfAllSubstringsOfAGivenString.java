88d81be903034674500b5d19c5a587fcb7fb8242
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-14 07:09:32 +0530

    Advanced DSA and Algorithms: 2D DP: CountUniqueCharactersOfAllSubstringsOfAGivenString: in-progress v2

--- b_2D_DP_dynamic_programming/CountUniqueCharactersOfAllSubstringsOfAGivenString.java
+++ b_2D_DP_dynamic_programming/CountUniqueCharactersOfAllSubstringsOfAGivenString.java
@@ -1,5 +1,17 @@
 public class CountUniqueCharactersOfAllSubstringsOfAGivenString {
     public int uniqueLetterString(String s) {
+        int n = s.length();
+        int[][] pos = new int[26][2];
 
+        for (int i = 0; i < 26; i++) {
+            pos[i][0] = -1;
+            pos[i][1] = -1;
+        }
+
+        int result = 0;
+
+
+
+        return result;
     }
 }
