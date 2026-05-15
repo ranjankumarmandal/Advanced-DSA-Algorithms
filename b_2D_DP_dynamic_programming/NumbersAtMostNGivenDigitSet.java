5c2b4898c9fe493395228af036b291fbb2920d94
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-15 10:52:59 +0530

    Advanced DSA and Algorithms: 2D DP: NumbersAtMostNGivenDigitSet: in-progress v1

--- b_2D_DP_dynamic_programming/NumbersAtMostNGivenDigitSet.java
+++ b_2D_DP_dynamic_programming/NumbersAtMostNGivenDigitSet.java
@@ -1,5 +1,16 @@
 public class NumbersAtMostNGivenDigitSet {
     public int atMostNGivenDigitSet(String[] digits, int n) {
+        String s = String.valueOf(n);
+        int m = s.length();
+        int d = digits.length;
+        int ans = 0;
 
+        for (int len = 1; len < m; len++) {
+            ans += Math.pow(d, len);
+        }
+
+
+
+        return ans + 1;
     }
 }
