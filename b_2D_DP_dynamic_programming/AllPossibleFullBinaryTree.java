8183307bb38df1bf9901422a33bb42587a86d4d0
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-15 11:25:04 +0530

    Advanced DSA and Algorithms: 2D DP: AllPossibleFullBinaryTree: completed

--- b_2D_DP_dynamic_programming/AllPossibleFullBinaryTree.java
+++ b_2D_DP_dynamic_programming/AllPossibleFullBinaryTree.java
@@ -21,7 +21,17 @@ public class AllPossibleFullBinaryTree {
         for (int left = 1; left < n; left += 2) {
             int right = n - 1 - left;
 
+            List<TreeNode> leftTrees = allPossibleFBT(left);
+            List<TreeNode> rightTrees = allPossibleFBT(right);
 
+            for (TreeNode l : leftTrees) {
+                for (TreeNode r : rightTrees) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
         }
 
         memo.put(n, res);
