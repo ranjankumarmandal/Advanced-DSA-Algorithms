26461d0f042a707accc84ea24d0bdd09affde813
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-15 09:44:51 +0530

    Advanced DSA and Algorithms: 2D DP: ValidPermutationOfDISequence: in-progress v1

--- b_2D_DP_dynamic_programming/ValidPermutationOfDISequence.java
+++ b_2D_DP_dynamic_programming/ValidPermutationOfDISequence.java
@@ -1,5 +1,18 @@
 public class ValidPermutationOfDISequence {
     public int numPermsDISequence(String s) {
+        int mod = 1000000007;
+        int n = s.length();
+        int[][] dp = new int [n+1][n+1];
+        Arrays.fill(dp[0],1);
 
+        f
+
+        int ans = 0;
+        for(int x:dp[n]){
+            ans += x;
+            ans %= mod;
+        }
+
+        return ans;
     }
 }
