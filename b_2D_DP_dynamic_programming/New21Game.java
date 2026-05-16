e1c223564f70df9a22c9f5d6976cb9c5be1d9f73
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-16 12:18:34 +0530

    Advanced DSA and Algorithms: 2D DP: New21Game: in-progress v1

--- b_2D_DP_dynamic_programming/New21Game.java
+++ b_2D_DP_dynamic_programming/New21Game.java
@@ -1,5 +1,14 @@
 public class New21Game {
     public double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts - 1) return 1.0;
 
+        double[] dp = new double[n + 1];
+        dp[0] = 1.0;
+
+        double windowSum = 1.0;
+        double result = 0.0;
+
+
+        return result;
     }
 }
