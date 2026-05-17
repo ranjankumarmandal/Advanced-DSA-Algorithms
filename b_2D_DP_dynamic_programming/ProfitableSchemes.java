3f5a19a2ab8b85d247102146812d9d2c8d120924
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-17 12:46:38 +0530

    Advanced DSA: 2D DP: ProfitableSchemes: in-progress v1

--- b_2D_DP_dynamic_programming/ProfitableSchemes.java
+++ b_2D_DP_dynamic_programming/ProfitableSchemes.java
@@ -1,5 +1,23 @@
 public class ProfitableSchemes {
     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        int mod = 1_000_000_007;
+        int[][] dp = new int[n + 1][minProfit + 1];
+        dp[0][0] = 1;
 
+        for (int k = 0; k < group.length; k++) {
+            int g = group[k];
+            int p = profit[k];
+
+            for (int members = n; members >= g; members--) {
+                for (int prof = minProfit; prof >= 0; prof--) {
+                    int np = Math.min(minProfit, prof + p);
+                    dp[members][np] = (dp[members][np] + dp[members - g][prof]) % mod;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        return ans;
     }
 }
