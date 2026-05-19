0c543d9c6a4e53349911ae87bce02851391bf69c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-19 17:19:52 +0530

    Advanced DSA: 2D DP: TallestBillboard: completed

--- b_2D_DP_dynamic_programming/TallestBillboard.java
+++ b_2D_DP_dynamic_programming/TallestBillboard.java
@@ -7,6 +7,19 @@ public class TallestBillboard {
         java.util.Arrays.fill(dp, -1);
         dp[0] = 0;
 
+        for (int r : rods) {
+            int[] cur = dp.clone();
+
+            for (int d = 0; d <= sum - r; d++) {
+                if (cur[d] < 0) continue;
+
+                dp[d + r] = Math.max(dp[d + r], cur[d]);
+
+                int nd = Math.abs(d - r);
+                dp[nd] = Math.max(dp[nd], cur[d] + Math.min(d, r));
+            }
+        }
+
         return dp[0];
     }
 }
