0754b77c3cf0a92672f2a922e73c39ec08807fd6
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-20 07:18:03 +0530

    Advanced DSA: 2D DP: MaxSubArraySumWithOneDeletion: completed

--- b_2D_DP_dynamic_programming/MaxSubArraySumWithOneDeletion.java
+++ b_2D_DP_dynamic_programming/MaxSubArraySumWithOneDeletion.java
@@ -9,7 +9,19 @@ public class MaxSubArraySumWithOneDeletion {
         left[0] = arr[0];
         int ans = arr[0];
 
+        for (int i = 1; i < n; i++) {
+            sum = Math.max(arr[i], sum + arr[i]);
+            left[i] = sum;
+            ans = Math.max(ans, sum);
+        }
 
+        sum = arr[n - 1];
+        right[n - 1] = arr[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            sum = Math.max(arr[i], sum + arr[i]);
+            right[i] = sum;
+        }
 
         for (int i = 1; i < n - 1; i++) {
             ans = Math.max(ans, left[i - 1] + right[i + 1]);
