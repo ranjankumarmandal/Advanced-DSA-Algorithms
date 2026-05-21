e672a84f6e557992818080a70cd48808c1314483
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-21 07:20:42 +0530

    Advanced DSA: 2D DP: CountNumberOfTeams: completed

--- b_2D_DP_dynamic_programming/CountNumberOfTeams.java
+++ b_2D_DP_dynamic_programming/CountNumberOfTeams.java
@@ -7,7 +7,10 @@ public class CountNumberOfTeams {
             int leftLess = 0, leftGreater = 0;
             int rightLess = 0, rightGreater = 0;
 
-
+            for (int i = 0; i < j; i++) {
+                if (rating[i] < rating[j]) leftLess++;
+                else if (rating[i] > rating[j]) leftGreater++;
+            }
 
             for (int k = j + 1; k < n; k++) {
                 if (rating[k] < rating[j]) rightLess++;
