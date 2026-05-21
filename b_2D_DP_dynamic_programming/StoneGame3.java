4152a62b6f4b76efc5966bb0baee53cad9f0e788
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-21 07:15:15 +0530

    Advanced DSA: 2D DP: StoneGame3: in-progress v2

--- b_2D_DP_dynamic_programming/StoneGame3.java
+++ b_2D_DP_dynamic_programming/StoneGame3.java
@@ -3,6 +3,9 @@ public class StoneGame3 {
         int n = stoneValue.length;
         int[] dp = new int[n + 1];
 
-
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Integer.MIN_VALUE;
+            int sum = 0;
+        }
     }
 }
