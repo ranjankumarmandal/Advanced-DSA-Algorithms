1419563f29ae7ea44447e76c1966e0ee7ecb8fcc
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-21 07:10:58 +0530

    Advanced DSA: 2D DP: RestoreArray: in-progress v1

--- b_2D_DP_dynamic_programming/RestoreArray.java
+++ b_2D_DP_dynamic_programming/RestoreArray.java
@@ -1,5 +1,12 @@
 public class RestoreArray {
     public int numberOfArrays(String s, int k) {
+        int n = s.length();
+        int mod = 1000000007;
+        long[] dp = new long[n + 1];
+        dp[n] = 1;
 
+
+
+        return (int) dp[0];
     }
 }
