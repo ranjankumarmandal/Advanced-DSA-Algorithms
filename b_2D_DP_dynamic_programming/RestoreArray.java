76abd518f2af9d67f8e3f3d8b8238e4c619dc30b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-21 07:12:25 +0530

    Advanced DSA: 2D DP: RestoreArray: completed

--- b_2D_DP_dynamic_programming/RestoreArray.java
+++ b_2D_DP_dynamic_programming/RestoreArray.java
@@ -10,6 +10,13 @@ public class RestoreArray {
 
             long num = 0;
 
+            for (int j = i; j < n; j++) {
+                num = num * 10 + (s.charAt(j) - '0');
+
+                if (num > k) break;
+
+                dp[i] = (dp[i] + dp[j + 1]) % mod;
+            }
         }
 
         return (int) dp[0];
