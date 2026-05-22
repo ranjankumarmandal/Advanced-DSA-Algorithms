cbb22a9b2c2965cb940a070ec51af6eb5437b0d9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-22 13:31:47 +0530

    Advanced DSA: 2D DP: SmallestSufficientTeam: in-progress v1

--- b_2D_DP_dynamic_programming/SmallestSufficientTeam.java
+++ b_2D_DP_dynamic_programming/SmallestSufficientTeam.java
@@ -1,5 +1,36 @@
 public class SmallestSufficientTeam {
     public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
+        int n = req_skills.length;
+        Map<String, Integer> skillMap = new HashMap<>();
 
+        for (int i = 0; i < n; i++) {
+            skillMap.put(req_skills[i], i);
+        }
+
+        int[] personMask = new int[people.size()];
+
+        for (int i = 0; i < people.size(); i++) {
+            int mask = 0;
+            for (String skill : people.get(i)) {
+                if (skillMap.containsKey(skill)) {
+                    mask |= (1 << skillMap.get(skill));
+                }
+            }
+            personMask[i] = mask;
+        }
+
+        List<Integer>[] dp = new ArrayList[1 << n];
+        dp[0] = new ArrayList<>();
+
+
+
+        List<Integer> ans = dp[(1 << n) - 1];
+        int[] res = new int[ans.size()];
+
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+
+        return res;
     }
 }
