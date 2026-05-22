ea04751deebdf253cc0e9713fd461d128e0b4419
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-22 19:16:32 +0530

    Advanced DSA: 2D DP: MinHeightShelves: in-progress v3

--- b_2D_DP_dynamic_programming/MinHeightShelves.java
+++ b_2D_DP_dynamic_programming/MinHeightShelves.java
@@ -4,6 +4,10 @@ public class MinHeightShelves {
         int[] dp = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+
+            int width = 0;
+            int height = 0;
 
 
         }
