136194b696a274945e054fbe55ab816d35365aa7
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-22 13:19:01 +0530

    Advanced DSA: 2D DP: ShortestCommonSupersequence: in-progress v1

--- b_2D_DP_dynamic_programming/ShortestCommonSupersequence.java
+++ b_2D_DP_dynamic_programming/ShortestCommonSupersequence.java
@@ -1,5 +1,22 @@
 public class ShortestCommonSupersequence {
     public String shortestCommonSupersequence(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
 
+        int[][] dp = new int[n + 1][m + 1];
+
+
+
+        while (i > 0) {
+            sb.append(str1.charAt(i - 1));
+            i--;
+        }
+
+        while (j > 0) {
+            sb.append(str2.charAt(j - 1));
+            j--;
+        }
+
+        return sb.reverse().toString();
     }
 }
