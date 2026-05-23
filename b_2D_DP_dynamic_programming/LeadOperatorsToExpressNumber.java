bbd785d7f656fc0ddcddc4db713c6017b2772070
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-23 10:40:45 +0530

    Advanced DSA: 2D DP: LeadOperatorsToExpressNumber: in-progress v1

--- b_2D_DP_dynamic_programming/LeadOperatorsToExpressNumber.java
+++ b_2D_DP_dynamic_programming/LeadOperatorsToExpressNumber.java
@@ -1,5 +1,25 @@
 public class LeadOperatorsToExpressNumber {
-    public int leastOpsExpressTarget(int x, int target) {
+    private Map<String, Integer> memo;
 
+    public int leastOpsExpressTarget(int x, int target) {
+        memo = new HashMap<>();
+        return dfs(x, target) - 1;
+    }
+
+    private int dfs(long x, long target) {
+        if (target == 0) return 0;
+
+        String key = x + "_" + target;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        if (target < x) {
+            int ops = (int) (2 * target);
+            ops = Math.min(ops, (int) (2 * (x - target) + 1));
+            memo.put(key, ops);
+            return ops;
+        }
+
+        memo.put(key, ops);
+        return ops;
     }
 }
