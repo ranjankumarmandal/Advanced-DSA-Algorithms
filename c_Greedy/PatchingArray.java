ec78e5466c7a772474d3e4d83788f0fe004f2568
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-25 08:16:09 +0530

    Advanced DSA: Greedy: PatchingArray: in-progress v1

--- c_Greedy/PatchingArray.java
+++ c_Greedy/PatchingArray.java
@@ -1,5 +1,16 @@
 public class PatchingArray {
     public int minPatches(int[] nums, int n) {
+        long reach = 1;
+        int patches = 0;
+        int i = 0;
 
+        while (reach <= n) {
+            if (i < nums.length && nums[i] <= reach) {
+                reach += nums[i];
+                i++;
+            }
+        }
+
+        return patches;
     }
 }
