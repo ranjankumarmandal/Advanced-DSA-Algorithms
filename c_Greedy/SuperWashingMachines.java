741107204c4ff1c4587f2a5b22fb0e76a8deec0b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-26 12:06:27 +0530

    Advanced DSA & Algorithms: SuperWashingMachines: in-progress v1

--- c_Greedy/SuperWashingMachines.java
+++ c_Greedy/SuperWashingMachines.java
@@ -1,5 +1,17 @@
 public class SuperWashingMachines {
     public int findMinMoves(int[] machines) {
+        int total = 0;
 
+        for (int dresses : machines) {
+            total += dresses;
+        }
+
+        int n = machines.length;
+
+        if (total % n != 0) {
+            return -1;
+        }
+
+        return moves;
     }
 }
