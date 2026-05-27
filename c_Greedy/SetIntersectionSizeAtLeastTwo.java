ab407c5df7705971ae881c646a30954e25053f60
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-27 19:15:32 +0530

    Advanced DSA & Algorithms: SetIntersectionSizeAtLeastTww: in-progress v1

--- c_Greedy/SetIntersectionSizeAtLeastTwo.java
+++ c_Greedy/SetIntersectionSizeAtLeastTwo.java
@@ -1,5 +1,12 @@
 public class SetIntersectionSizeAtLeastTwo {
     public int intersectionSizeTwo(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[1] == b[1]) return b[0] - a[0];
+            return a[1] - b[1];
+        });
 
+
+
+        return ans;
     }
 }
