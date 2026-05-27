01bb4f6886e692190dbbb133b56e71359957bc4e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-27 19:26:04 +0530

    Advanced DSA & Algorithms: Greedy: ValidTriangleNumber: in-progress v4

--- c_Greedy/ValidTriangleNumber.java
+++ c_Greedy/ValidTriangleNumber.java
@@ -8,7 +8,9 @@ public class ValidTriangleNumber {
             int i = 0, j = k - 1;
 
             while (i < j) {
-
+                if (nums[i] + nums[j] > nums[k]) {
+                    ans += j - i;
+                }
             }
         }
 
