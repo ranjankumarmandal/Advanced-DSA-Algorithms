63f3b27e61c7290b165b413f54963405f20e3a4f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-28 09:05:08 +0530

    Advanced DSA & Algorithms: Greedy: MaxDistanceInArrays: in-progress v1

--- c_Greedy/MaxDistanceInArrays.java
+++ c_Greedy/MaxDistanceInArrays.java
@@ -1,5 +1,9 @@
 public class MaxDistanceInArrays {
     public int maxDistance(List<List<Integer>> arrays) {
+        int min = arrays.get(0).get(0);
+        int max = arrays.get(0).get(arrays.get(0).size() - 1);
+        int ans = 0;
 
+        return ans;
     }
 }
