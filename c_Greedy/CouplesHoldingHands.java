fad020668e79579147d7659ed3e1697418d690e2
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-29 14:35:41 +0530

    Advanced DSA & Algorithms: Greedy: CouplesHoldingHands: in-progress v2

--- c_Greedy/CouplesHoldingHands.java
+++ c_Greedy/CouplesHoldingHands.java
@@ -3,6 +3,10 @@ public class CouplesHoldingHands {
         int n = row.length;
         int[] pos = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            pos[row[i]] = i;
+        }
+
 
 
         return swaps;
