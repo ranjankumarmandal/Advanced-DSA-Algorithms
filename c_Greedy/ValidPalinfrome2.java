c6c4915d62b3d2e5e72581e8f2bb82f54f71f843
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-01 23:11:40 +0530

    Advanced DSA & Algorithms: Greedy: ValidPalinfrome2: in-progress v2

--- c_Greedy/ValidPalinfrome2.java
+++ c_Greedy/ValidPalinfrome2.java
@@ -2,6 +2,14 @@ public class ValidPalinfrome2 {
     public boolean validPalindrome(String s) {
         int left = 0, right = s.length() - 1;
 
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
         return true;
     }
 }
