9ac8bac24e84669bb5d2a4e5bdd5ef2ac4ea7de4
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-01 19:24:06 +0530

    Advanced DSA & Algorithms: MaxChunksToMakeSorted2: in-progress v1

--- c_Greedy/MaxChunksToMakeSorted2.java
+++ c_Greedy/MaxChunksToMakeSorted2.java
@@ -1,4 +1,8 @@
 public class MaxChunksToMakeSorted2 {
     public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int[] rightMin = new int[n + 1];
+
+        return chunks;
     }
 }
