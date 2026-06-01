4bb286666f6dfa2921e26bd180a705fdc1fbd7fb
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-01 20:27:35 +0530

    Advanced DSA & Algorithms: MonotoneIncreasingDigits: in-pgoress v2

--- c_Greedy/MonotoneIncreasingDigits.java
+++ c_Greedy/MonotoneIncreasingDigits.java
@@ -2,5 +2,10 @@ public class MonotoneIncreasingDigits {
     public int monotoneIncreasingDigits(int n) {
         char[] digits = String.valueOf(n).toCharArray();
         int marker = digits.length;
+
+        for (int i = digits.length - 1; i > 0; i--) {
+            if (digits[i - 1] > digits[i]) {
+            }
+        }
     }
 }
