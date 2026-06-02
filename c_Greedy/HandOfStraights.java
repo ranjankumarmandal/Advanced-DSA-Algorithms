bcf6c88dd6a6158e1aab17755f25df455c9ed7ff
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-02 08:34:02 +0530

    Advanced DSA & Algorithms: Greedy: HandOfStraights: in-progress v3

--- c_Greedy/HandOfStraights.java
+++ c_Greedy/HandOfStraights.java
@@ -5,6 +5,10 @@ public class HandOfStraights {
         int n = hand.length;
         if (n % groupSize != 0) return false;
 
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int card : hand) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
 
     }
 }
