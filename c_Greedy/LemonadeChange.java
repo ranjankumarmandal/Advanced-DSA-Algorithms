7b75fb350ebab8226ce95bd447ed15cb408bdafb
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-02 08:40:47 +0530

    Advanced DSA & Algorithms: Greedy: LemonadeChange: in-progress v2

--- c_Greedy/LemonadeChange.java
+++ c_Greedy/LemonadeChange.java
@@ -2,7 +2,11 @@ public class LemonadeChange {
     public boolean lemonadeChange(int[] bills) {
         int five = 0, ten = 0;
 
-
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            }
+        }
 
         return true;
     }
