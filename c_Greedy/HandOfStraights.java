816b675866e11b673e5854f1460bcf6b67716a97
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-02 08:36:09 +0530

    Advanced DSA & Algorithms: Greedy: HandOfStraights: in-progress v4

--- c_Greedy/HandOfStraights.java
+++ c_Greedy/HandOfStraights.java
@@ -10,5 +10,21 @@ public class HandOfStraights {
             map.put(card, map.getOrDefault(card, 0) + 1);
         }
 
+        while (!map.isEmpty()) {
+            int start = map.firstKey();
+
+            for (int i = 0; i < groupSize; i++) {
+                int card = start + i;
+                Integer count = map.get(card);
+
+                if (count == null) return false;
+
+                if (count == 1) {
+                    map.remove(card);
+                } else {
+                    map.put(card, count - 1);
+                }
+            }
+        }
     }
 }
