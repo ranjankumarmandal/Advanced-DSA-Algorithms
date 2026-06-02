cb762c23effa91e2624a2813a5db813763511556
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-02 08:25:40 +0530

    Advanced DSA & Algorithms: Greedy: MinCostToHireKWorkders: in-progress v2

--- c_Greedy/MinCostToHireKWorkders.java
+++ c_Greedy/MinCostToHireKWorkders.java
@@ -2,5 +2,7 @@ import java.util.*;
 
 public class MinCostToHireKWorkders {
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+        int n = quality.length;
+        double[][] workers = new double[n][2];
     }
 }
