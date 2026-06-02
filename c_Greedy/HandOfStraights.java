0e9fbb84667b8b9e807a94a6011c3b28417f1125
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-02 08:33:44 +0530

    Advanced DSA & Algorithms: Greedy: HandOfStraights: in-progress v2

--- c_Greedy/HandOfStraights.java
+++ c_Greedy/HandOfStraights.java
@@ -2,5 +2,9 @@ import java.util.*;
 
 public class HandOfStraights {
     public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+        if (n % groupSize != 0) return false;
+
+
     }
 }
