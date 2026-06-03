207b498fe0f384d5e518ee36e8c49943697b831c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-03 07:13:03 +0530

    Advanced DSA & Algorthms: Greedy: MinNumOfRefuelingStops: in-progress v1

--- c_Greedy/MinNumOfRefuelingStops.java
+++ c_Greedy/MinNumOfRefuelingStops.java
@@ -1,12 +1,4 @@
 public class MinNumOfRefuelingStops {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        long fuel = startFuel;
-        int prev = 0;
-        int stops = 0;
-
-
-
-        return stops;
     }
 }
