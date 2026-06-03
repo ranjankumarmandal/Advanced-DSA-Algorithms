e3747b2cfac98520f0866af86a1a84f646e40ff5
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-03 07:23:34 +0530

    Advanced DSA & Algorthms: Greedy: AdvantageShuffle: in-progress v1

--- c_Greedy/AdvantageShuffle.java
+++ c_Greedy/AdvantageShuffle.java
@@ -1,4 +1,12 @@
 public class AdvantageShuffle {
     public int[] advantageCount(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        Arrays.sort(nums1);
+
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums2[i];
+            arr[i][1] = i;
+        }
     }
 }
