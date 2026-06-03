c6d0501e312b904e85a28c9e719c55840848042b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-03 07:27:56 +0530

    Advanced DSA & Algorthms: Greedy: ScoreAfterFlippingMatrix: in-progress v3

--- c_Greedy/ScoreAfterFlippingMatrix.java
+++ c_Greedy/ScoreAfterFlippingMatrix.java
@@ -2,5 +2,8 @@ public class ScoreAfterFlippingMatrix {
     public int matrixScore(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        int ans = 0;
+
+        return ans;
     }
 }
