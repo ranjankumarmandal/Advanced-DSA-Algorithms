070d6aefc252e694a06301f810eb23d4d34297e4
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-04 07:07:27 +0530

    Advanced DSA & Algorthms: Greedy: StampingTheSequence: in-progress v1

--- c_Greedy/StampingTheSequence.java
+++ c_Greedy/StampingTheSequence.java
@@ -1,4 +1,12 @@
+import java.util.*;
+
 public class StampingTheSequence {
     public int[] movesToStamp(String stamp, String target) {
+        char[] s = stamp.toCharArray();
+        char[] t = target.toCharArray();
+        int m = s.length, n = t.length;
+        boolean[] visited = new boolean[n];
+        int stars = 0;
+        List<Integer> res = new ArrayList<>();
     }
 }
