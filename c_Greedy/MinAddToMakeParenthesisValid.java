915c3dc1941e0be10655fa40049f746b30c27362
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-04 07:20:16 +0530

    Advanced DSA & Algorthms: Greedy: MinAddToMakeParenthesisValid: in-progress v2

--- c_Greedy/MinAddToMakeParenthesisValid.java
+++ c_Greedy/MinAddToMakeParenthesisValid.java
@@ -2,5 +2,9 @@ import java.util.*;
 
 public class MinAddToMakeParenthesisValid {
     public int minAddToMakeValid(String s) {
+        int balance = 0;
+        int additions = 0;
+
+        return additions + balance;
     }
 }
