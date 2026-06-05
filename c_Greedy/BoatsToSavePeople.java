3d9206ba84327d42668f4a708a8ac0edf89b7ec7
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-05 07:53:22 +0530

    Advanced DSA & Algorthms: Greedy: BoatsToSavePeople: in-progress v2

--- c_Greedy/BoatsToSavePeople.java
+++ c_Greedy/BoatsToSavePeople.java
@@ -1,5 +1,7 @@
 public class BoatsToSavePeople {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int boats = 0;
     }
 }
