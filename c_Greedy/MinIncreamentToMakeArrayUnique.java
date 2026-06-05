00c5cf98d1b9bb1db378c46592c1944f014ce852
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-05 08:00:22 +0530

    Advanced DSA & Algorthms: Greedy: MinIncreamentToMakeArrayUnique: completed

--- c_Greedy/MinIncreamentToMakeArrayUnique.java
+++ c_Greedy/MinIncreamentToMakeArrayUnique.java
@@ -5,6 +5,9 @@ public class MinIncreamentToMakeArrayUnique {
 
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] <= nums[i - 1]) {
+                int target = nums[i - 1] + 1;
+                moves += target - nums[i];
+                nums[i] = target;
             }
         }
 
