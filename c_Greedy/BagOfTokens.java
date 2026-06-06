235d104667e5f4d8b002db32ea7a89badea2294b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-06 22:49:54 +0530

    Advanced DSA & Algorthms: Greedy: BagOfTokens: in-progress v1

--- c_Greedy/BagOfTokens.java
+++ c_Greedy/BagOfTokens.java
@@ -1,4 +1,10 @@
 public class BagOfTokens {
     public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int s = 0;
+        int maxi = 0;
+        int l = 0, r = tokens.length - 1;
+
+        return maxi;
     }
 }
