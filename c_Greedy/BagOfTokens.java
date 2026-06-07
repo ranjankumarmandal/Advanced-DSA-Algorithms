08a525f3ba23c0a4c1ebf0c8684d1aed0996f53e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-07 23:44:56 +0530

    Advanced DSA & Algorthms: Greedy: LargestNumber

--- c_Greedy/BagOfTokens.java
+++ c_Greedy/BagOfTokens.java
@@ -5,6 +5,21 @@ public class BagOfTokens {
         int maxi = 0;
         int l = 0, r = tokens.length - 1;
 
+        while (l <= r) {
+            if (power >= tokens[l]) {
+                power -= tokens[l];
+                s++;
+                l++;
+                maxi = Math.max(maxi, s);
+            } else if (s > 0) {
+                power += tokens[r];
+                s--;
+                r--;
+            } else {
+                break;
+            }
+        }
+
         return maxi;
     }
 }
