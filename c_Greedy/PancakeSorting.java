09fe6110c791c8ca677cb4276d1893f4426b83eb
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-08 23:53:58 +0530

    Advanced DSA & Algorthms: Greedy: PancakeSorting: in-progress v1

--- c_Greedy/PancakeSorting.java
+++ c_Greedy/PancakeSorting.java
@@ -1,4 +1,23 @@
 public class PancakeSorting {
     public List<Integer> pancakeSort(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int n = arr.length;
+        int[] indices = new int[n];
+
+        for(int i=0; i<n; ++i) indices[arr[i]-1] = i;
+        for(int i=0; i<n; ++i) {
+            int pull = indices[i];
+            dew(stack, i, pull);
+            indices[arr[i]-1] = pull;
+            indices[i] = i;
+            arr[pull] = arr[i];
+            arr[i] = i+1;
+        }
+        LinkedList<Integer> ans = new LinkedList<>();
+        while(!stack.isEmpty()) {
+            int x = stack.pop();
+            ans.addFirst(x);
+        }
+        return ans;
     }
 }
