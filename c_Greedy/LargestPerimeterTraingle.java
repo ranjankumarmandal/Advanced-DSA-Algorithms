05c33c5d7cb6203dc1ea68e6e13d362578b7070a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-09 08:34:08 +0530

    Advanced DSA & Algorthms: Greedy: LargestPerimeterTraingle: in-progress v2

--- c_Greedy/LargestPerimeterTraingle.java
+++ c_Greedy/LargestPerimeterTraingle.java
@@ -1,5 +1,8 @@
 public class LargestPerimeterTraingle {
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
+
+        for (int i = nums.length - 1; i >= 2; i--) {
+        }
     }
 }
