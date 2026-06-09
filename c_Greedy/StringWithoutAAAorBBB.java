40718234ef64d1c26dba7fbd24999df944a0e99a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-09 08:39:44 +0530

    Advanced DSA & Algorthms: Greedy: StringWithoutAAAorBBB: in-progress v2

--- c_Greedy/StringWithoutAAAorBBB.java
+++ c_Greedy/StringWithoutAAAorBBB.java
@@ -2,6 +2,11 @@ public class StringWithoutAAAorBBB {
     public String strWithout3a3b(int a, int b) {
         StringBuilder sb = new StringBuilder();
 
+        while (a > 0 || b > 0) {
+            int n = sb.length();
+
+
+        }
 
     }
 }
