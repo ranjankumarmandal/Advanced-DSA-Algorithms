e472c28663ca5913a76fef73748db211b3a952ce
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-10 07:09:34 +0530

    Advanced DSA & Algorthms: Greedy: MinDominoRotationsForEqualRow: in-progress v2

--- c_Greedy/MinDominoRotationsForEqualRow.java
+++ c_Greedy/MinDominoRotationsForEqualRow.java
@@ -2,5 +2,10 @@ public class MinDominoRotationsForEqualRow {
     public int minDominoRotations(int[] tops, int[] bottoms) {
         int n = tops.length, res = Integer.MAX_VALUE;
         int[] face = new int[7];
+
+        for (int i = 0; i < n; i++) {
+            face[tops[i]]++;
+            if (bottoms[i] != tops[i]) face[bottoms[i]]++;
+        }
     }
 }
