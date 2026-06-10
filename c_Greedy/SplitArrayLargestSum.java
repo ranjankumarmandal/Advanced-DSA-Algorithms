98d17c105a0e935e44101ef14d927a80bf528a4e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-10 06:55:04 +0530

    Advanced DSA & Algorthms: Greedy: SplitArrayLargestSum: in-progress v2

--- c_Greedy/SplitArrayLargestSum.java
+++ c_Greedy/SplitArrayLargestSum.java
@@ -1,5 +1,10 @@
 public class SplitArrayLargestSum {
     int[] nums;
 
-    public int splitArray(int[] nums, int m) {}
+    public int splitArray(int[] nums, int m) {
+        this.nums = nums;
+        int low = 0, high = 0, min = Integer.MAX_VALUE;
+
+        return min;
+    }
 }
