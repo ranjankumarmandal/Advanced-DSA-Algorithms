2475953d216a1c673b3c3f1b9e0767fd7c3fa80d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-10 07:02:32 +0530

    Advanced DSA & Algorthms: Greedy: MaximiseSumOfArrayAfterKNegation: in-progress v1

--- c_Greedy/MaximiseSumOfArrayAfterKNegation.java
+++ c_Greedy/MaximiseSumOfArrayAfterKNegation.java
@@ -1,4 +1,7 @@
 public class MaximiseSumOfArrayAfterKNegation {
     public int largestSumAfterKNegations(int[] nums, int k) {
+        int[] numbers = new int[201];
+        int sum = 0;
+        int maxAbs = 0;
     }
 }
