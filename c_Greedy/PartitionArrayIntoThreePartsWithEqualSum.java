6505bee8c82cd2100acafaef639de1752aaa399c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-11 07:18:17 +0530

    Advanced DSA & Algorthms: Greedy: PartitionArrayIntoThreePartsWithEqualSum: in-progress v2

--- c_Greedy/PartitionArrayIntoThreePartsWithEqualSum.java
+++ c_Greedy/PartitionArrayIntoThreePartsWithEqualSum.java
@@ -1,5 +1,8 @@
 public class PartitionArrayIntoThreePartsWithEqualSum {
     public boolean canThreePartsEqualSum(int[] arr) {
         int total = 0;
+        for (int num : arr) {
+            total += num;
+        }
     }
 }
