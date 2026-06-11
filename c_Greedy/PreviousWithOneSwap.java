a1d0f0cc87cfe46094646b60224b24cbf37a6668
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-11 07:10:46 +0530

    Advanced DSA & Algorthms: Greedy: PreviousWithOneSwap: in-progress v3

--- c_Greedy/PreviousWithOneSwap.java
+++ c_Greedy/PreviousWithOneSwap.java
@@ -6,5 +6,12 @@ public class PreviousWithOneSwap {
         while (i >= 0 && arr[i] <= arr[i + 1]) {
             i--;
         }
+
+        if (i < 0) return arr;
+
+        int j = n - 1;
+        while (arr[j] >= arr[i]) {
+            j--;
+        }
     }
 }
