b22678680f5bc1f4059556eb4a2558b05c8cfea7
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-12 08:25:45 +0530

    Advanced DSA & Algorthms: Greedy: SmallestSubsquenceOfDistinctChars: in-progress v1

--- c_Greedy/SmallestSubsquenceOfDistinctChars.java
+++ c_Greedy/SmallestSubsquenceOfDistinctChars.java
@@ -1,4 +1,8 @@
 public class SmallestSubsquenceOfDistinctChars {
     public String smallestSubsequence(String s) {
+        int[] last = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
     }
 }
