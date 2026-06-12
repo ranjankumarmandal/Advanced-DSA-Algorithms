f49c30edc5748a664ecdd16928047bafb9df8d95
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-12 08:20:55 +0530

    Advanced DSA & Algorthms: Greedy: DistantBarcodes: in-progress v1

--- c_Greedy/DistantBarcodes.java
+++ c_Greedy/DistantBarcodes.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
 public class DistantBarcodes {
-    public int[] rearrangeBarcodes(int[] barcodes) {}
+    public int[] rearrangeBarcodes(int[] barcodes) {
+        Map<Integer, Integer> freq = new HashMap<>();
+    }
 }
