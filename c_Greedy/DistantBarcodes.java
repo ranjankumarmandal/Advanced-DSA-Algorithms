9aac2ea4ed955061d1d387dfca797286179d0806
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-12 08:21:41 +0530

    Advanced DSA & Algorthms: Greedy: DistantBarcodes: in-progress v4

--- c_Greedy/DistantBarcodes.java
+++ c_Greedy/DistantBarcodes.java
@@ -11,5 +11,19 @@ public class DistantBarcodes {
         for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
             pq.offer(new int[]{e.getKey(), e.getValue()});
         }
+
+        int[] ans = new int[barcodes.length];
+        int idx = 0;
+
+        while (pq.size() >= 2) {
+            int[] a = pq.poll();
+            int[] b = pq.poll();
+
+            ans[idx++] = a[0];
+            ans[idx++] = b[0];
+
+            if (--a[1] > 0) pq.offer(a);
+            if (--b[1] > 0) pq.offer(b);
+        }
     }
 }
