55df7731481bcf5fdaa2d867bd0ac5c74f6ad04a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-12 08:31:18 +0530

    Advanced DSA & Algorthms: Greedy: MinCostTreeFromLeafValues: in-progress v3

--- c_Greedy/MinCostTreeFromLeafValues.java
+++ c_Greedy/MinCostTreeFromLeafValues.java
@@ -5,5 +5,12 @@ public class MinCostTreeFromLeafValues {
         int res = 0;
         Deque<Integer> st = new ArrayDeque<>();
         st.push(Integer.MAX_VALUE);
+
+        for (int x : arr) {
+            while (st.peek() <= x) {
+                int mid = st.pop();
+                res += mid * Math.min(st.peek(), x);
+            }
+        }
     }
 }
