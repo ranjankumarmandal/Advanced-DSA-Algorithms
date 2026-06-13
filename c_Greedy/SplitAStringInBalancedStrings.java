ee0315fdc13d8ae9dc4215e3c43e569045b85c54
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-13 11:36:29 +0530

    Advanced DSA & Algorthms: Greedy: SplitAStringInBalancedStrings: in-progress v2

--- c_Greedy/SplitAStringInBalancedStrings.java
+++ c_Greedy/SplitAStringInBalancedStrings.java
@@ -1,5 +1,8 @@
 public class SplitAStringInBalancedStrings {
     public int balancedStringSplit(String s) {
         int balance = 0, count = 0;
+
+        for (char c : s.toCharArray()) {
+        }
     }
 }
