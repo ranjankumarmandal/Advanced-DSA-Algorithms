e614d9fe6bd99b2383d710b4f45c1a96e06d5d3c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-13 09:20:54 +0530

    Advanced DSA & Algorthms: Greedy: MinCostToMoveChipsToTheSamePosition: in-progress v2

--- c_Greedy/MinCostToMoveChipsToTheSamePosition.java
+++ c_Greedy/MinCostToMoveChipsToTheSamePosition.java
@@ -1,5 +1,8 @@
 public class MinCostToMoveChipsToTheSamePosition {
     ublic int minCostToMoveChips(int[] position) {
         int even = 0, odd = 0;
+
+        for (int p : position) {
+        }
     }
 }
