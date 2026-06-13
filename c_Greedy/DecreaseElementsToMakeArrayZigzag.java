432f4ee35ba12f4c0b21c5772c828422ae193f03
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-13 09:17:17 +0530

    Advanced DSA & Algorthms: Greedy: DecreaseElementsToMakeArrayZigzag: in-progress v2

--- c_Greedy/DecreaseElementsToMakeArrayZigzag.java
+++ c_Greedy/DecreaseElementsToMakeArrayZigzag.java
@@ -2,5 +2,9 @@ public class DecreaseElementsToMakeArrayZigzag {
     public int movesToMakeZigzag(int[] nums) {
         int n = nums.length;
         long[] cost = new long[2];
+
+        for (int i = 0; i < n; i++) {
+            int limit = Integer.MAX_VALUE;
+        }
     }
 }
