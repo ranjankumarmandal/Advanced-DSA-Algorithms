a888703d95c30498bcd8d801034df921202f4248
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-14 11:28:01 +0530

    Advanced DSA & Algorithms: Greedy: ReconstructA2RowBinaryMatrix: in-progress v1

--- c_Greedy/ReconstructA2RowBinaryMatrix.java
+++ c_Greedy/ReconstructA2RowBinaryMatrix.java
@@ -1,4 +1,7 @@
 public class ReconstructA2RowBinaryMatrix {
     public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
+        int n = colsum.length;
+        int[] top = new int[n];
+        int[] bottom = new int[n];
     }
 }
