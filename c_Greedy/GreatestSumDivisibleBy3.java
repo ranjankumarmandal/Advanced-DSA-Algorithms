959c44ff945c006f9e856a6e81008b1a6edcedf1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-14 11:32:05 +0530

    Advanced DSA & Algorithms: Greedy: GreatestSumDivisibleBy3: in-progress v2

--- c_Greedy/GreatestSumDivisibleBy3.java
+++ c_Greedy/GreatestSumDivisibleBy3.java
@@ -1,5 +1,9 @@
 public class GreatestSumDivisibleBy3 {
     public int maxSumDivThree(int[] nums) {
         int[] dp = {0, Integer.MIN_VALUE, Integer.MIN_VALUE};
+
+        for (int num : nums) {
+            int[] next = dp.clone();
+        }
     }
 }
