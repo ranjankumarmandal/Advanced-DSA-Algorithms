e17a56fa8ea3903ba4386f25a211bbd3d4b7dda8
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-14 11:23:04 +0530

    Advanced DSA & Algorithms: Greedy: MinSwapsToMakeStringsEqual: in-progress v2

--- c_Greedy/MinSwapsToMakeStringsEqual.java
+++ c_Greedy/MinSwapsToMakeStringsEqual.java
@@ -1,5 +1,11 @@
 public class MinSwapsToMakeStringsEqual {
     public int minimumSwap(String s1, String s2) {
         int xy = 0, yx = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == 'x' && s2.charAt(i) == 'y') {
+                xy++;
+            }
+        }
     }
 }
