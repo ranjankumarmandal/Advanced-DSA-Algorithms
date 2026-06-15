63e0234af4d0c3afc08bcde410350bcd094e5f18
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-15 11:57:31 +0530

    Advanced DSA & Algorithms: Greedy: Max69Number: in-progress v4

--- c_Greedy/Max69Number.java
+++ c_Greedy/Max69Number.java
@@ -4,6 +4,8 @@ public class Max69Number {
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == '6') {
+                arr[i] = '9';
+                break;
             }
         }
     }
