a653f78212ead5d0d78d444ed9643582a307cf79
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-15 11:34:51 +0530

    Advanced DSA & Algorithms: Greedy: GroupThePeople: in-progress v1

--- c_Greedy/GroupThePeople.java
+++ c_Greedy/GroupThePeople.java
@@ -1,4 +1,6 @@
 public class GroupThePeople {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<List<Integer>> ans = new ArrayList<>();
     }
 }
