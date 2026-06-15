33d7dd80eabcdb41e4ee510c596c40c2117b5855
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-15 11:53:14 +0530

    Advanced DSA & Algorithms: Greedy: DivideArrayInSetsOfKConsecutiveNumbers: in-progress v1

--- c_Greedy/DivideArrayInSetsOfKConsecutiveNumbers.java
+++ c_Greedy/DivideArrayInSetsOfKConsecutiveNumbers.java
@@ -1,4 +1,7 @@
 public class DivideArrayInSetsOfKConsecutiveNumbers {
     public boolean isPossibleDivide(int[] nums, int k) {
+        if (nums.length % k != 0) return false;
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
     }
 }
