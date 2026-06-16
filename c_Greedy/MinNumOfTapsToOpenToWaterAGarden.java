16d5b4a862a7e9da8c7a9e232f09523db27f5a02
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-16 07:22:18 +0530

    Advanced DSA & Algorithms: Greedy: MinNumOfTapsToOpenToWaterAGarden: in-progress v2

--- c_Greedy/MinNumOfTapsToOpenToWaterAGarden.java
+++ c_Greedy/MinNumOfTapsToOpenToWaterAGarden.java
@@ -2,5 +2,10 @@ public class MinNumOfTapsToOpenToWaterAGarden {
     public int minTaps(int n, int[] ranges) {
         int[] maxReach = new int[n + 1];
 
+        for (int i = 0; i <= n; i++) {
+            int left = Math.max(0, i - ranges[i]);
+            int right = Math.min(n, i + ranges[i]);
+            maxReach[left] = Math.max(maxReach[left], right);
+        }
     }
 }
