3b8e00644a10b8948655eece16a459c8d2d14223
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-16 07:28:14 +0530

    Advanced DSA & Algorithms: Greedy: ReverseSubArrayToMaximizeArrayValue: in-progress v2

--- c_Greedy/ReverseSubArrayToMaximizeArrayValue.java
+++ c_Greedy/ReverseSubArrayToMaximizeArrayValue.java
@@ -2,5 +2,10 @@ public class ReverseSubArrayToMaximizeArrayValue {
     public int maxValueAfterReverse(int[] nums) {
         int n = nums.length;
         int base = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            base += Math.abs(nums[i] - nums[i + 1]);
+        }
+
     }
 }
