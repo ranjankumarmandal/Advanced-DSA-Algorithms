d46fa4af41cd0eace32b036bf65d5f5dc2adc993
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-17 07:14:09 +0530

    Advanced DSA & Algorithms: Greedy: Maximum69NumV2 v4

--- c_Greedy/Maximum69NumV2.java
+++ c_Greedy/Maximum69NumV2.java
@@ -2,5 +2,12 @@ public class Maximum69NumV2 {
     public int maximum69Number(int num) {
         char[] digits = String.valueOf(num).toCharArray();
 
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break;
+            }
+        }
+
     }
 }
