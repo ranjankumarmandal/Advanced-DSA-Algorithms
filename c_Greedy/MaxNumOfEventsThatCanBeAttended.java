1e5681fd7ea9ed4f4849d1e4e4ebddecc25a4ef5
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-17 07:04:50 +0530

    Advanced DSA & Algorithms: Greedy: MaxNumOfEventsThatCanBeAttended: in-progress v2

--- c_Greedy/MaxNumOfEventsThatCanBeAttended.java
+++ c_Greedy/MaxNumOfEventsThatCanBeAttended.java
@@ -1,5 +1,9 @@
 public class MaxNumOfEventsThatCanBeAttended {
     public int maxEvents(int[][] events) {
         Arrays.sort(events, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = events.length;
+        int i = 0;
     }
 }
