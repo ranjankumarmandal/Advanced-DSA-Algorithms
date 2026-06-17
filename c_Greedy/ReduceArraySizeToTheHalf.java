0e9072d835b924f15acdef7c9554521aa4e47933
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-17 07:10:12 +0530

    Advanced DSA & Algorithms: Greedy: ReduceArraySizeToTheHalf: in-progress v3

--- c_Greedy/ReduceArraySizeToTheHalf.java
+++ c_Greedy/ReduceArraySizeToTheHalf.java
@@ -4,5 +4,9 @@ public class ReduceArraySizeToTheHalf {
     public int minSetSize(int[] arr) {
         Map<Integer, Integer> freq = new HashMap<>();
 
+        for (int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
     }
 }
