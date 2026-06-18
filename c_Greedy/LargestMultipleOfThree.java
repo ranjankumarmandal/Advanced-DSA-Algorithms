d993366256c2c5f031bc02d83bd3af570dc5d207
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-18 06:59:26 +0530

    Advanced DSA & Algorithms: Greedy: LargestMultipleOfThree: in-progress v2

--- c_Greedy/LargestMultipleOfThree.java
+++ c_Greedy/LargestMultipleOfThree.java
@@ -2,5 +2,11 @@ public class LargestMultipleOfThree {
     public String largestMultipleOfThree(int[] digits) {
         int[] cnt = new int[10];
         int sum = 0;
+        for (int d : digits) {
+            cnt[d]++;
+            sum += d;
+        }
+
+        int mod = sum % 3;
     }
 }
