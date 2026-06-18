d4102af3a24a49e10ff4ca7568b2beb892996f4d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-18 07:09:01 +0530

    Advanced DSA & Algorithms: Greedy: MaxPerformanceOfATeam: in-progress v1

--- c_Greedy/MaxPerformanceOfATeam.java
+++ c_Greedy/MaxPerformanceOfATeam.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
 public class MaxPerformanceOfATeam {
+    private static final int MOD = 1_000_000_007;
 
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+    }
 }
