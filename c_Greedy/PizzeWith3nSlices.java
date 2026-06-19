eec977a8e410982153d6c2fe6fcacbeda4ad6b6b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-19 20:27:43 +0530

    Advanced DSA & Algorithms: Greedy: PizzeWith3nSlices: in-progress v2

--- c_Greedy/PizzeWith3nSlices.java
+++ c_Greedy/PizzeWith3nSlices.java
@@ -1,5 +1,7 @@
 public class PizzeWith3nSlices {
     public int maxSizeSlices(int[] slices) {
         int m = slices.length / 3;
+        return Math.max(calc(slices, 0, slices.length - 2, m),
+                calc(slices, 1, slices.length - 1, m));
     }
 }
