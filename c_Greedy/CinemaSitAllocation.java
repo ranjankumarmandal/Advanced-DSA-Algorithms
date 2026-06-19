e3fbdebb29920066bb7635aaa701184e32a41416
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-19 20:34:30 +0530

    Advanced DSA & Algorithms: Greedy: CinemaSitAllocation: in-progress v3

--- c_Greedy/CinemaSitAllocation.java
+++ c_Greedy/CinemaSitAllocation.java
@@ -4,5 +4,12 @@ public class CinemaSitAllocation {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
         Map<Integer, Integer> map = new HashMap<>();
 
+        for (int[] seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
+            if (col >= 2 && col <= 9) {
+                map.put(row, map.getOrDefault(row, 0) | (1 << col));
+            }
+        }
     }
 }
