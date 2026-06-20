96ae3ab476d2fde5349607226f684b443b73b1cc
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-20 15:30:07 +0530

    Advanced DSA & Algorithms: Greedy: LongestHappyString: in-progress v2

--- c_Greedy/LongestHappyString.java
+++ c_Greedy/LongestHappyString.java
@@ -2,5 +2,7 @@ import java.util.*;
 
 public class LongestHappyString {
     public String longestDiverseString(int a, int b, int c) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> y[0] - x[0]);
+
     }
 }
