23da84ca355153027bc4b3d0c8b665d2adb8596f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-20 15:36:36 +0530

    Advanced DSA & Algorithms: Greedy: MinSubsequenceInIncreasingOrder: in-progress v3

--- c_Greedy/MinSubsequenceInIncreasingOrder.java
+++ c_Greedy/MinSubsequenceInIncreasingOrder.java
@@ -4,5 +4,8 @@ public class MinSubsequenceInIncreasingOrder {
     public List<Integer> minSubsequence(int[] nums) {
         Arrays.sort(nums);
 
+        int total = 0;
+        for (int num : nums) total += num;
+
     }
 }
