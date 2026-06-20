e58c1972a18519f13af4661d93206bec66dad777
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-20 15:24:34 +0530

    Advanced DSA & Algorithms: Greedy: ReducingDishes: in-progress v3

--- c_Greedy/ReducingDishes.java
+++ c_Greedy/ReducingDishes.java
@@ -4,5 +4,7 @@ public class ReducingDishes {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
         int suffixSum = 0;
+        int result = 0;
+
     }
 }
