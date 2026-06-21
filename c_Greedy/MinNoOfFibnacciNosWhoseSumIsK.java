9984a3a964736286c942c9b611f8c84884bfd787
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-21 13:12:45 +0530

    Advanced DSA & Algorithms: Greedy: MinNoOfFibnacciNosWhoseSumIsK: in-progress v1

--- c_Greedy/MinNoOfFibnacciNosWhoseSumIsK.java
+++ c_Greedy/MinNoOfFibnacciNosWhoseSumIsK.java
@@ -1,4 +1,6 @@
 public class MinNoOfFibnacciNosWhoseSumIsK {
     public int findMinFibonacciNumbers(int k) {
+        java.util.List<Integer> fib = new java.util.ArrayList<>();
+        fib.add(1);
     }
 }
