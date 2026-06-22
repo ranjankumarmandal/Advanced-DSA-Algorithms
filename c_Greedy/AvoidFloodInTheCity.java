745d3193f4583a6cc46cbdaee688c0757fe0c502
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-22 20:04:05 +0530

    Advanced DSA & Algorithms: Greedy: AvoidFloodInTheCity: in-progress v2

--- c_Greedy/AvoidFloodInTheCity.java
+++ c_Greedy/AvoidFloodInTheCity.java
@@ -2,5 +2,8 @@ public class AvoidFloodInTheCity {
     public int[] avoidFlood(int[] rains) {
         int n = rains.length;
         int[] ans = new int[n];
+        Map<Integer, Integer> lastRain = new HashMap<>();
+        TreeSet<Integer> dryDays = new TreeSet<>();
+
     }
 }
