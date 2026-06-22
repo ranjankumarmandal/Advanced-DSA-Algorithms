1dfb3dbe35ff42047e820218eadd1133980b6ed8
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-22 19:52:52 +0530

    Advanced DSA & Algorithms: Greedy: MaxAreaAreaAfterHorizontalAndVerticalCut: in-progress v2

--- c_Greedy/MaxAreaAreaAfterHorizontalAndVerticalCut.java
+++ c_Greedy/MaxAreaAreaAfterHorizontalAndVerticalCut.java
@@ -2,5 +2,7 @@ public class MaxAreaAreaAfterHorizontalAndVerticalCut {
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
         Arrays.sort(horizontalCuts);
         Arrays.sort(verticalCuts);
+
+        long maxH = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length - 1]);
     }
 }
