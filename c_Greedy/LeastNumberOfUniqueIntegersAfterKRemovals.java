3e0b17371d24f718e33b3977fb1ca513d4008656
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-22 19:59:59 +0530

    Advanced DSA & Algorithms: Greedy: LeastNumberOfUniqueIntegersAfterKRemovals: in-progress v1

--- c_Greedy/LeastNumberOfUniqueIntegersAfterKRemovals.java
+++ c_Greedy/LeastNumberOfUniqueIntegersAfterKRemovals.java
@@ -1,4 +1,6 @@
 public class LeastNumberOfUniqueIntegersAfterKRemovals {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
     }
 }
