a0b5fefe8cc0d041e2755059e55a5432956c333c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-23 15:00:20 +0530

    Advanced DSA & Algorithms: Greedy: MaxNumOfNonOverlappingSubString: in-progress v2

--- c_Greedy/MaxNumOfNonOverlappingSubString.java
+++ c_Greedy/MaxNumOfNonOverlappingSubString.java
@@ -2,5 +2,8 @@ public class MaxNumOfNonOverlappingSubString {
     public List<String> maxNumOfSubstrings(String s) {
         int n = s.length();
 
+        int[] left = new int[26];
+        int[] right = new int[26];
+
     }
 }
