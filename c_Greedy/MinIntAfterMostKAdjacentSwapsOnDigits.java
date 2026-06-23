e75d2469f71c2c2d789d072809e1b6b049b15dc8
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-23 14:06:59 +0530

    Advanced DSA & Algorithms: Greedy: MinIntAfterMostKAdjacentSwapsOnDigits: in-progress v2

--- c_Greedy/MinIntAfterMostKAdjacentSwapsOnDigits.java
+++ c_Greedy/MinIntAfterMostKAdjacentSwapsOnDigits.java
@@ -2,6 +2,12 @@ public class MinIntAfterMostKAdjacentSwapsOnDigits {
     static class Fenwick {
         int n;
         int[] bit;
+
+        Fenwick(int n) {
+            this.n = n;
+            bit = new int[n + 2];
+        }
+
     }
 
     public String minInteger(String num, int k) {}
