bb881bf85326c7c392f67d99eb7bf2f1625810e4
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-23 14:15:17 +0530

    Advanced DSA & Algorithms: Greedy: MinDiffBtwnLargestAndSmallestValueInThreeMoves: in-progress v4

--- c_Greedy/MinDiffBtwnLargestAndSmallestValueInThreeMoves.java
+++ c_Greedy/MinDiffBtwnLargestAndSmallestValueInThreeMoves.java
@@ -7,5 +7,8 @@ public class MinDiffBtwnLargestAndSmallestValueInThreeMoves {
 
         int ans = Integer.MAX_VALUE;
 
+        ans = Math.min(ans, nums[n - 4] - nums[0]);
+        ans = Math.min(ans, nums[n - 3] - nums[1]);
+        ans = Math.min(ans, nums[n - 2] - nums[2]);
     }
 }
