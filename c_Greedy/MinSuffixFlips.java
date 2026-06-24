8a6c635fa86f7f584b5aef3b3cd1dc9bf99738ff
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-24 07:38:20 +0530

    Advanced DSA & Algorithms: Greedy: MinSuffixFlips: in-progress v2

--- c_Greedy/MinSuffixFlips.java
+++ c_Greedy/MinSuffixFlips.java
@@ -2,5 +2,8 @@ public class MinSuffixFlips {
     public int minFlips(String target) {
         int flips = 0;
         char current = '0';
+
+        for (char c : target.toCharArray()) {
+        }
     }
 }
