8b684e9475171958fdd93c2af7aa0f2e20303335
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-24 07:41:32 +0530

    Advanced DSA & Algorithms: Greedy: MinSwapsToArrangeABinaryGrid: in-progress v4

--- c_Greedy/MinSwapsToArrangeABinaryGrid.java
+++ c_Greedy/MinSwapsToArrangeABinaryGrid.java
@@ -13,5 +13,25 @@ public class MinSwapsToArrangeABinaryGrid {
 
         int swaps = 0;
 
+        for (int i = 0; i < n; i++) {
+            int need = n - 1 - i;
+            int j = i;
+
+            while (j < n && zeros[j] < need) {
+                j++;
+            }
+
+            if (j == n) {
+                return -1;
+            }
+
+            while (j > i) {
+                int temp = zeros[j];
+                zeros[j] = zeros[j - 1];
+                zeros[j - 1] = temp;
+                swaps++;
+                j--;
+            }
+        }
     }
 }
