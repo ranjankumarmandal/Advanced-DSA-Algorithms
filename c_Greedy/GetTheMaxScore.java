44e1e6aaf72d2a0cc69e03112b074c3f4b6cfc7c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-25 07:06:40 +0530

    Advanced DSA & Algorithms: Greedy: GetTheMaxScore: in-progress v1

--- c_Greedy/GetTheMaxScore.java
+++ c_Greedy/GetTheMaxScore.java
@@ -1,5 +1,8 @@
 public class GetTheMaxScore {
     public int maxSum(int[] nums1, int[] nums2) {
         long mod = 1000000007L;
+        int i = 0, j = 0;
+        long s1 = 0, s2 = 0;
+
     }
 }
