70ae321982c879e776998bf04714289d753b7a26
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-25 07:15:01 +0530

    Advanced DSA & Algorithms: Greedy: MaxNonOverlappingSubarraysWithSumEqualTarget: in-progress v1

--- c_Greedy/MaxNonOverlappingSubarraysWithSumEqualTarget.java
+++ c_Greedy/MaxNonOverlappingSubarraysWithSumEqualTarget.java
@@ -1,4 +1,6 @@
 public class MaxNonOverlappingSubarraysWithSumEqualTarget {
     ublic int maxNonOverlapping(int[] nums, int target) {
+        Set<Integer> set = new HashSet<>();
+        set.add(0);
     }
 }
