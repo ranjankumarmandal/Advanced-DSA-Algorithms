85af9a3c85beca5a509eb46c4800c88e2e40f047
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-26 08:25:23 +0530

    Advanced DSA & Algorithms: Greedy: MaxNumOfCoins: in-progress v3

--- c_Greedy/MaxNumOfCoins.java
+++ c_Greedy/MaxNumOfCoins.java
@@ -4,5 +4,8 @@ public class MaxNumOfCoins {
     public int maxCoins(int[] piles) {
         Arrays.sort(piles);
         int n = piles.length;
+        int ans = 0;
+        int l = 0, r = n - 2;
+
     }
 }
