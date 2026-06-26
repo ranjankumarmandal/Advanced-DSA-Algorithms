bd9a6ecc81ff8aaf20b1bcf535d7a20316b2da43
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-26 08:19:11 +0530

    Advanced DSA & Algorithms: Greedy: MinNumOfFuncCallsToMakeTargetArray: in-progress v4

--- c_Greedy/MinNumOfFuncCallsToMakeTargetArray.java
+++ c_Greedy/MinNumOfFuncCallsToMakeTargetArray.java
@@ -3,6 +3,13 @@ public class MinNumOfFuncCallsToMakeTargetArray {
         int add = 0, dbl = 0;
 
         for (int x : nums) {
+            int bits = Integer.bitCount(x);
+            add += bits;
+            int shifts = 0;
+            while (x > 1) {
+                x >>= 1;
+                shifts++;
+            }
         }
     }
 }
