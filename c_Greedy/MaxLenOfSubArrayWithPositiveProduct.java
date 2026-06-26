6ea9c83d39a9d3991a0b685602d801ef33e726d3
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-26 08:36:31 +0530

    Advanced DSA & Algorithms: Greedy: MaxLenOfSubArrayWithPositiveProduct: in-progress v2

--- c_Greedy/MaxLenOfSubArrayWithPositiveProduct.java
+++ c_Greedy/MaxLenOfSubArrayWithPositiveProduct.java
@@ -2,5 +2,11 @@ public class MaxLenOfSubArrayWithPositiveProduct {
     public int getMaxLen(int[] nums) {
         int pos = 0, neg = 0, ans = 0;
 
+        for (int x : nums) {
+            if (x == 0) {
+                pos = 0;
+                neg = 0;
+            }
+        }
     }
 }
