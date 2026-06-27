227c6390c8a17490cd2c8e503ad47b7eced6dedf
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-27 07:27:42 +0530

    Advanced DSA & Algorithms: Graph: EvaluateDecision: in-progress v1

--- d_Graph/EvaluateDecision.java
+++ d_Graph/EvaluateDecision.java
@@ -1,4 +1,6 @@
 public class EvaluateDecision {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, List<Pair>> graph = new HashMap<>();
+
     }
 }
