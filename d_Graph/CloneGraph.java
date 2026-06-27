3eb1253daf9eed18d90e540c0a3949e3ac0155a1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-27 07:16:03 +0530

    Advanced DSA & Algorithms: Graph: CloneGraph: in-progress v1

--- d_Graph/CloneGraph.java
+++ d_Graph/CloneGraph.java
@@ -1,4 +1,7 @@
 public class CloneGraph {
     public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        Map<Node, Node> map = new HashMap<>();
+        Queue<Node> queue = new LinkedList<>();
     }
 }
