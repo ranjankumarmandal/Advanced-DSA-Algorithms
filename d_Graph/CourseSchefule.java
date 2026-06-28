ffa0cc0b7f071d6752762aa30bcc5e9cf4772634
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-28 07:47:41 +0530

    Advanced DSA & Algorithms: Graph: CourseSchefule: in-progress v1

--- d_Graph/CourseSchefule.java
+++ d_Graph/CourseSchefule.java
@@ -1,4 +1,7 @@
 public class CourseSchefule {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
+
     }
 }
