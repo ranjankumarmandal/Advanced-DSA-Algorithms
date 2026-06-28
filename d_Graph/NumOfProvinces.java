298bc1b3c979daaa0182210fc8f8df836c367554
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-28 07:57:31 +0530

    Advanced DSA & Algorithms: Graph: NumOfProvinces: in-progress v1

--- d_Graph/NumOfProvinces.java
+++ d_Graph/NumOfProvinces.java
@@ -1,4 +1,7 @@
 public class NumOfProvinces {
     public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
     }
 }
