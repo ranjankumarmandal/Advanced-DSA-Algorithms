9fb7a54d57f107f2ab4b797be8599401096c4f25
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-29 08:57:24 +0530

    Advanced DSA & Algorithms: Graph: RedundantConnection2: in-progress v2

--- d_Graph/RedundantConnection2.java
+++ d_Graph/RedundantConnection2.java
@@ -3,5 +3,16 @@ public class RedundantConnection2 {
         int n = edges.length;
         int[] parent = new int[n + 1];
         int[] cand1 = null, cand2 = null;
+
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            if (parent[v] == 0) {
+                parent[v] = u;
+            } else {
+                cand1 = new int[]{parent[v], v};
+                cand2 = new int[]{u, v};
+                edge[1] = 0;
+            }
+        }
     }
 }
