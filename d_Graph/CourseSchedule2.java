13c90f06f2b06279c0ea5f0fd4ea3bd539d95407
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-29 09:06:18 +0530

    Advanced DSA & Algorithms: Graph: CourseSchedule2: in-progress v1

--- d_Graph/CourseSchedule2.java
+++ d_Graph/CourseSchedule2.java
@@ -1,4 +1,6 @@
 public class CourseSchedule2 {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        List<Integer>[] graph = new ArrayList[numCourses];
     }
 }
