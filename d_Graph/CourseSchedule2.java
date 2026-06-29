b00221dc615c3de46eb5714b64937f94c2266ba9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-29 09:07:08 +0530

    Advanced DSA & Algorithms: Graph: CourseSchedule2: in-progress v3

--- d_Graph/CourseSchedule2.java
+++ d_Graph/CourseSchedule2.java
@@ -11,5 +11,12 @@ public class CourseSchedule2 {
             inDegree[pair[0]]++;
             graph[pair[1]].add(pair[0]);
         }
+
+        Queue<Integer> toVisit = new ArrayDeque<>();
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0) {
+                toVisit.offer(i);
+            }
+        }
     }
 }
