daa15a5f926ea36116699354611538f3d1e1c59d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-30 13:29:59 +0530

    Advanced DSA & Algorithms: Graph: KeysAndRooms: in-progress v1

--- KeysAndRooms.java
+++ KeysAndRooms.java
@@ -1,4 +1,6 @@
 public class KeysAndRooms {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        Deque<Integer> stack = new ArrayDeque<>();
     }
 }
