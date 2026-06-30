fc60b9842dc27567fe270744282e0c248b196d83
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-30 13:37:48 +0530

    Advanced DSA & Algorithms: Graph: ReachableNodesInSubDividedGraph: in-progress v3

--- d_Graph/ReachableNodesInSubDividedGraph.java
+++ d_Graph/ReachableNodesInSubDividedGraph.java
@@ -4,5 +4,9 @@ public class ReachableNodesInSubDividedGraph {
     public int reachableNodes(int[][] edges, int maxMoves, int n) {
         List<int[]>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] e : edges) {
+            graph[e[0]].add(new int[]{e[1], e[2]});
+            graph[e[1]].add(new int[]{e[0], e[2]});
+        }
     }
 }
