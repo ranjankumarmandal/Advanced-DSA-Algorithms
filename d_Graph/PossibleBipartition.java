f600f3ba6e43641204f914b333b66e0b0f834676
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-30 13:43:28 +0530

    Advanced DSA & Algorithms: Graph: PossibleBipartition: in-progress v3

--- d_Graph/PossibleBipartition.java
+++ d_Graph/PossibleBipartition.java
@@ -4,5 +4,10 @@ public class PossibleBipartition {
     public boolean possibleBipartition(int n, int[][] dislikes) {
         List<Integer>[] graph = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+
+        for (int[] d : dislikes) {
+            graph[d[0]].add(d[1]);
+            graph[d[1]].add(d[0]);
+        }
     }
 }
