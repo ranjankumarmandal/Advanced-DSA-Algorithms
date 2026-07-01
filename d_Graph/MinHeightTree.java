b0a0cca410c452c5f803cf40691040646458e35e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-01 07:17:07 +0530

    Advanced DSA & Algorithms: Graph: MinHeightTree: in-progress v2

--- d_Graph/MinHeightTree.java
+++ d_Graph/MinHeightTree.java
@@ -2,5 +2,8 @@ public class MinHeightTree {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         if (n == 1) return Arrays.asList(0);
 
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+
     }
 }
