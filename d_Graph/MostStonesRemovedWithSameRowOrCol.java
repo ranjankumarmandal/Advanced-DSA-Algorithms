8e6c0c1eefbadad45e8f7d7edcaa8828aee7bc60
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-02 07:11:40 +0530

    Advanced DSA & Algorithms: Graph: MostStonesRemovedWithSameRowOrCol: in-progress v2

--- d_Graph/MostStonesRemovedWithSameRowOrCol.java
+++ d_Graph/MostStonesRemovedWithSameRowOrCol.java
@@ -2,5 +2,7 @@ public class MostStonesRemovedWithSameRowOrCol {
     int[] parent;
 
     public int removeStones(int[][] stones) {
+        int n = stones.length;
+        parent = new int[n];
     }
 }
