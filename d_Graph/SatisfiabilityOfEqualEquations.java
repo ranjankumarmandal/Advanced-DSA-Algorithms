34e23ac31a75b63707883056405c43a160aa216c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-02 07:16:13 +0530

    Advanced DSA & Algorithms: Graph: SatisfiabilityOfEqualEquations: in-progress v4

--- d_Graph/SatisfiabilityOfEqualEquations.java
+++ d_Graph/SatisfiabilityOfEqualEquations.java
@@ -4,5 +4,10 @@ public class SatisfiabilityOfEqualEquations {
     public boolean equationsPossible(String[] equations) {
         for (int i = 0; i < 26; i++) parent[i] = i;
 
+        for (String e : equations) {
+            if (e.charAt(1) == '=') {
+                union(e.charAt(0) - 'a', e.charAt(3) - 'a');
+            }
+        }
     }
 }
