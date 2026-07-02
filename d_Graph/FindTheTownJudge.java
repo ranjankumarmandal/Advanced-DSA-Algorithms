907b90d2b1f407181fb246475ef94980c316bc0c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-02 07:08:35 +0530

    Advanced DSA & Algorithms: Graph: FindTheTownJudge: completed

--- d_Graph/FindTheTownJudge.java
+++ d_Graph/FindTheTownJudge.java
@@ -5,10 +5,13 @@ public class FindTheTownJudge {
             score[t[0]]--;
             score[t[1]]++;
         }
+
         for (int i = 1; i <= n; i++) {
             if (score[i] == n - 1) {
                 return i;
             }
         }
+
+        return -1;
     }
 }
