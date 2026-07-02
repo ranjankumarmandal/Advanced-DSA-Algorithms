33f0d48f167e6fc5c75ed0636b4019c4c130ea92
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-02 07:12:57 +0530

    Advanced DSA & Algorithms: Graph: MostStonesRemovedWithSameRowOrCol: completed

--- d_Graph/MostStonesRemovedWithSameRowOrCol.java
+++ d_Graph/MostStonesRemovedWithSameRowOrCol.java
@@ -23,4 +23,12 @@ public class MostStonesRemovedWithSameRowOrCol {
         if (parent[x] != x) parent[x] = find(parent[x]);
         return parent[x];
     }
+
+    boolean union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa == pb) return false;
+        parent[pa] = pb;
+        return true;
+    }
 }
