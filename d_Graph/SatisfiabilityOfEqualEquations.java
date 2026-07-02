8383332ea75b434ad9d5b5ce1af4e8a55d953962
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-02 07:15:57 +0530

    Advanced DSA & Algorithms: Graph: SatisfiabilityOfEqualEquations: in-progress v3

--- d_Graph/SatisfiabilityOfEqualEquations.java
+++ d_Graph/SatisfiabilityOfEqualEquations.java
@@ -2,5 +2,7 @@ public class SatisfiabilityOfEqualEquations {
     int[] parent = new int[26];
 
     public boolean equationsPossible(String[] equations) {
+        for (int i = 0; i < 26; i++) parent[i] = i;
+
     }
 }
