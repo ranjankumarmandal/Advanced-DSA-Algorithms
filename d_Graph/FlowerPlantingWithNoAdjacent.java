72743afc632cab8b8f614b875fed8f450ae5627d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-03 12:24:32 +0530

    Advanced DSA & Algorithms: FlowerPlantingWithNoAdjacent: in-progress

--- d_Graph/FlowerPlantingWithNoAdjacent.java
+++ d_Graph/FlowerPlantingWithNoAdjacent.java
@@ -1,4 +1,6 @@
 public class FlowerPlantingWithNoAdjacent {
     public int[] gardenNoAdj(int n, int[][] paths) {
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
     }
 }
