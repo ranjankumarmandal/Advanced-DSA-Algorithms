42796498da8c643b7544eda3fa042bfd32db258f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-03 12:29:56 +0530

    Advanced DSA & Algorithms: SortedPathWithAlternativeColors: in-progress v1

--- d_Graph/SortedPathWithAlternativeColors.java
+++ d_Graph/SortedPathWithAlternativeColors.java
@@ -2,5 +2,8 @@ import java.util.*;
 
 public class SortedPathWithAlternativeColors {
     public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
+        List<Integer>[] red = new ArrayList[n];
+        List<Integer>[] blue = new ArrayList[n];
+
     }
 }
