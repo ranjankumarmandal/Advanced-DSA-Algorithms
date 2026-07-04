7225015e3b55679fc59ecbd32bbf896f473887c4
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-04 14:20:14 +0530

    Advanced DSA & Algorithms: Graph: ReconstructItenary: in-progress v2

--- d_Graph/ReconstructItenary.java
+++ d_Graph/ReconstructItenary.java
@@ -2,5 +2,10 @@ public class ReconstructItenary {
     public List<String> findItinerary(List<List<String>> tickets) {
         Map<String, PriorityQueue<String>> graph = new HashMap<>();
 
+        for (List<String> ticket : tickets) {
+            graph.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+            graph.get(ticket.get(0)).add(ticket.get(1));
+        }
+
     }
 }
