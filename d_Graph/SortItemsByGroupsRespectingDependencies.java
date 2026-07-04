3c2ee3abd31207b50fc082849951ad895010c21a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-04 14:57:11 +0530

    Advanced DSA & Algorithms: Graph: SortItemsByGroupsRespectingDependencies: in-progress v2

--- d_Graph/SortItemsByGroupsRespectingDependencies.java
+++ d_Graph/SortItemsByGroupsRespectingDependencies.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class SortItemsByGroupsRespectingDependencies {
     public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
+        int groupId = m;
+        for (int i = 0; i < n; i++) {
+            if (group[i] == -1) group[i] = groupId++;
+        }
 
     }
 }
