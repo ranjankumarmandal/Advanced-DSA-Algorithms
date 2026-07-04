91f01173a95115c8eae9244d46b71d1f4f138c16
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-04 14:46:12 +0530

    Advanced DSA & Algorithms: Graph: CriticalConnectionsInANetwork: in-progress v1

--- d_Graph/CriticalConnectionsInANetwork.java
+++ d_Graph/CriticalConnectionsInANetwork.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
 public class CriticalConnectionsInANetwork {
+    int time = 0;
 
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+
+    }
 }
