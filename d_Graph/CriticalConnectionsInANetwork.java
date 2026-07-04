854083a60932fda14ad9b19b130cb1c11fc79a28
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-04 14:46:37 +0530

    Advanced DSA & Algorithms: Graph: CriticalConnectionsInANetwork: in-progress v3

--- d_Graph/CriticalConnectionsInANetwork.java
+++ d_Graph/CriticalConnectionsInANetwork.java
@@ -7,5 +7,10 @@ public class CriticalConnectionsInANetwork {
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
 
+        for (List<Integer> e : connections) {
+            graph.get(e.get(0)).add(e.get(1));
+            graph.get(e.get(1)).add(e.get(0));
+        }
+
     }
 }
