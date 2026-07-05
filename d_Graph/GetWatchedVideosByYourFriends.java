b781c5d3fc78d63d5dc15867ffdc4b4a37ad8830
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-05 07:45:37 +0530

    Advanced DSA & Algorithms: Graph: GetWatchedVideosByYourFriends: in-progress v2

--- d_Graph/GetWatchedVideosByYourFriends.java
+++ d_Graph/GetWatchedVideosByYourFriends.java
@@ -2,5 +2,8 @@ public class GetWatchedVideosByYourFriends {
     public List<String> watchedVideosByFriends(List<List<String>> watchedVideos, int[][] friends, int id, int level) {
         int n = friends.length;
         boolean[] vis = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(id);
+        vis[id] = true;
     }
 }
