743c77731903e5721ddd09f519e09e63d1196e05
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-05 07:45:54 +0530

    Advanced DSA & Algorithms: Graph: GetWatchedVideosByYourFriends: in-progress v3

--- d_Graph/GetWatchedVideosByYourFriends.java
+++ d_Graph/GetWatchedVideosByYourFriends.java
@@ -5,5 +5,18 @@ public class GetWatchedVideosByYourFriends {
         Queue<Integer> q = new LinkedList<>();
         q.offer(id);
         vis[id] = true;
+
+        while (level-- > 0) {
+            int size = q.size();
+            while (size-- > 0) {
+                int u = q.poll();
+                for (int v : friends[u]) {
+                    if (!vis[v]) {
+                        vis[v] = true;
+                        q.offer(v);
+                    }
+                }
+            }
+        }
     }
 }
