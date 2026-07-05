10c20d24897ea14843daa063055e04a9540fbc86
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-05 07:41:07 +0530

    Advanced DSA & Algorithms: Graph: MaxCandiesYorCanTakeFromBoxes: in-progress v1

--- d_Graph/MaxCandiesYorCanTakeFromBoxes.java
+++ d_Graph/MaxCandiesYorCanTakeFromBoxes.java
@@ -1,5 +1,9 @@
 public class MaxCandiesYorCanTakeFromBoxes {
     public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        int n = status.length;
+        boolean[] haveBox = new boolean[n];
+        boolean[] opened = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
 
     }
 }
