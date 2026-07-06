e13c4c0fbd0bdc9d5af6b38583e966b612acb549
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-06 12:36:14 +0530

    Advanced DSA & Algorithms: FindTheCityWithSmallestNeighbors: in-progress v1

--- d_Graph/FindTheCityWithSmallestNeighbors.java
+++ d_Graph/FindTheCityWithSmallestNeighbors.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
 public class FindTheCityWithSmallestNeighbors {
-
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int INF = 1_000_000_000;
+        int[][] dist = new int[n][n];
+    }
 }
