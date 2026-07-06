a8d0b702cd2faaebf185b62a2d6ab0047a566cff
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-06 12:25:24 +0530

    Advanced DSA & Algorithms: ValidateBinaryTreeNodes: in-progress v3

--- d_Graph/ValidateBinaryTreeNodes.java
+++ d_Graph/ValidateBinaryTreeNodes.java
@@ -4,5 +4,15 @@ public class ValidateBinaryTreeNodes {
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
         int[] indeg = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            if (leftChild[i] != -1) {
+                indeg[leftChild[i]]++;
+                if (indeg[leftChild[i]] > 1) return false;
+            }
+            if (rightChild[i] != -1) {
+                indeg[rightChild[i]]++;
+                if (indeg[rightChild[i]] > 1) return false;
+            }
+        }
     }
 }
