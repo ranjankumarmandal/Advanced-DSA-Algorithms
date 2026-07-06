15c212dd0dd5cd0937af4ebc9a32e44810e97429
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-06 12:31:12 +0530

    Advanced DSA & Algorithms: NumOfOperationsToMakeNetworkConnected: in-progress v2

--- d_Graph/NumOfOperationsToMakeNetworkConnected.java
+++ d_Graph/NumOfOperationsToMakeNetworkConnected.java
@@ -3,4 +3,9 @@ import java.util.*;
 public class NumOfOperationsToMakeNetworkConnected {
     int[] parent;
     int[] rank;
+
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
 }
