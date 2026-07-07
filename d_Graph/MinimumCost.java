dffed0be331d4efe8a6a95d9ff91613eee3b6fca
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-07 13:45:45 +0530

    Advanced DSA & Algorithms: MinimumCost: in-progress v2

--- d_Graph/MinimumCost.java
+++ d_Graph/MinimumCost.java
@@ -2,5 +2,10 @@ public class MinimumCost {
     public int minCost(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         int[][] dist = new int[m][n];
+        for (int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
+
+        int[] dr = {0, 0, 1, -1};
+        int[] dc = {1, -1, 0, 0};
+
     }
 }
