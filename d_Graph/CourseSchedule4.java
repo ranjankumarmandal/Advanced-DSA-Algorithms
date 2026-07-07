a6878e154a9252bcc70adb310e6f4ee05b08a11b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-07 13:27:55 +0530

    Advanced DSA & Algorithms: CourseSchedule4: in-progress v1

--- d_Graph/CourseSchedule4.java
+++ d_Graph/CourseSchedule4.java
@@ -1,5 +1,7 @@
 public class CourseSchedule4 {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] indegree = new int[numCourses];
 
     }
 }
