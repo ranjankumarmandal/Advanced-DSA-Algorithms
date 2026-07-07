7d77f6a9a1264f6bd21bd72566f6cdd5530dc869
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-07 13:37:59 +0530

    Advanced DSA & Algorithms: FrogPositionAfteTSeconds: in-progress v2

--- d_Graph/FrogPositionAfteTSeconds.java
+++ d_Graph/FrogPositionAfteTSeconds.java
@@ -6,6 +6,8 @@ public class FrogPositionAfteTSeconds {
     double ans;
 
     public double frogPosition(int n, int[][] edges, int t, int target) {
-
+        this.target = target;
+        this.limit = t;
+        graph = new ArrayList[n + 1];
     }
 }
