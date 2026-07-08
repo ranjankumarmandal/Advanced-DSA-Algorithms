5713a7b77fc8671cfbf405ddd2b258e9fe1e00b5
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-08 07:07:03 +0530

    Advanced DSA & Algorithms: ParallelCourses2: in-progress v2

--- d_Graph/ParallelCourses2.java
+++ d_Graph/ParallelCourses2.java
@@ -4,5 +4,9 @@ public class ParallelCourses2 {
         for (int[] r : relations) {
             pre[r[1] - 1] |= 1 << (r[0] - 1);
         }
+        int m = 1 << n;
+        int[] dp = new int[m];
+        Arrays.fill(dp, n);
+        dp[0] = 0;
     }
 }
