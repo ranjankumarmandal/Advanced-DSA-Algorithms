b15336ba9fed85fbe8d3c8551eea105b5eb9f7b1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-08 07:16:32 +0530

    Advanced DSA & Algorithms: FindCriticalAndPseudoCriticalEdges: in-progress

--- d_Graph/FindCriticalAndPseudoCriticalEdges.java
+++ d_Graph/FindCriticalAndPseudoCriticalEdges.java
@@ -1,3 +1,16 @@
 public class FindCriticalAndPseudoCriticalEdges {
+    class DSU {
+        int[] p, r;
 
+        DSU(int n) {
+            p = new int[n];
+            r = new int[n];
+            for (int i = 0; i < n; i++) p[i] = i;
+        }
+
+        int find(int x) {
+            if (p[x] != x) p[x] = find(p[x]);
+            return p[x];
+        }
+    }
 }
