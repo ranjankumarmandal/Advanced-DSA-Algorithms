69efbbf199590045596d982e78bd00d5c0cc9059
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-08 07:12:50 +0530

    Advanced DSA & Algorithms: MinimumReorder: in-progress v2

--- d_Graph/MinimumReorder.java
+++ d_Graph/MinimumReorder.java
@@ -1,6 +1,10 @@
 public class MinimumReorder {
     public int minReorder(int n, int[][] connections) {
         List<int[]>[] graph = new ArrayList[n];
-
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] e : connections) {
+            graph[e[0]].add(new int[]{e[1], 1});
+            graph[e[1]].add(new int[]{e[0], 0});
+        }
     }
 }
