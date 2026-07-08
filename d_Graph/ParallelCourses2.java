b0730be19b3ab397317ce845d717675f62953491
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-08 07:06:46 +0530

    Advanced DSA & Algorithms: ParallelCourses2: in-progress v1

--- d_Graph/ParallelCourses2.java
+++ d_Graph/ParallelCourses2.java
@@ -1,5 +1,8 @@
 public class ParallelCourses2 {
     public int minNumberOfSemesters(int n, int[][] relations, int k) {
-
+        int[] pre = new int[n];
+        for (int[] r : relations) {
+            pre[r[1] - 1] |= 1 << (r[0] - 1);
+        }
     }
 }
