d69cbbb6e5e3abde2555c0e35cfa4cc5138f9b0f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-09 07:12:24 +0530

    Advanced DSA & Algorithms: MaximumNumEdgesToRemove: in-progress v2

--- d_Graph/MaximumNumEdgesToRemove.java
+++ d_Graph/MaximumNumEdgesToRemove.java
@@ -5,4 +5,11 @@ public class MaximumNumEdgesToRemove {
         if (parent[x] != x) parent[x] = find(parent[x]);
         return parent[x];
     }
+
+    boolean union(int a, int b) {
+        int pa = find(a), pb = find(b);
+        if (pa == pb) return false;
+        parent[pa] = pb;
+        return true;
+    }
 }
