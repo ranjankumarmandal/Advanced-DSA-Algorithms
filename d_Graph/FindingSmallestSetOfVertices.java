9a787ec4e1bfa137b9aa83730ccf7f9a98f6b2d1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-09 07:18:22 +0530

    Advanced DSA & Algorithms: FindingSmallestSetOfVertices: in-progress v1

--- d_Graph/FindingSmallestSetOfVertices.java
+++ d_Graph/FindingSmallestSetOfVertices.java
@@ -1,5 +1,5 @@
 public class FindingSmallestSetOfVertices {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-
+        boolean[] in = new boolean[n];
     }
 }
