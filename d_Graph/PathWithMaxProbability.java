b34179620dfb3ba4d6eb3a85ec2e5c4a5d900d8b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-09 07:24:07 +0530

    Advanced DSA & Algorithms: PathWithMaxProbability: in-progress v2

--- d_Graph/PathWithMaxProbability.java
+++ d_Graph/PathWithMaxProbability.java
@@ -3,5 +3,11 @@ public class PathWithMaxProbability {
         List<int[]>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
 
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0], v = edges[i][1];
+            graph[u].add(new int[]{v, i});
+            graph[v].add(new int[]{u, i});
+        }
+
     }
 }
