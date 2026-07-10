72c85d654830e29f15637f1d6ab32e87cc26e227
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-10 12:21:25 +0530

    Advanced DSA & Algorithms: MinCostToConnectAllPoints: in-progress v2

--- d_Graph/MinCostToConnectAllPoints.java
+++ d_Graph/MinCostToConnectAllPoints.java
@@ -3,5 +3,9 @@ public class MinCostToConnectAllPoints {
         int n = points.length;
         int[] minDist = new int[n];
         boolean[] vis = new boolean[n];
+        java.util.Arrays.fill(minDist, Integer.MAX_VALUE);
+        minDist[0] = 0;
+        int ans = 0;
+
     }
 }
