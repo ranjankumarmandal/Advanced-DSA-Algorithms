03db74e1f76e94f15db28fd995322aec9ad208ab
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-10 11:43:11 +0530

    Advanced DSA & Algorithms: StrangePrinter2: in-progress v1

--- d_Graph/StrangePrinter2.java
+++ d_Graph/StrangePrinter2.java
@@ -1,5 +1,7 @@
 public class StrangePrinter2 {
     public boolean isPrintable(int[][] targetGrid) {
-
+        int m = targetGrid.length;
+        int n = targetGrid[0].length;
+        int max = 60;
     }
 }
