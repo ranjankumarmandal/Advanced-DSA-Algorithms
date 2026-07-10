9205d701557ff4f865f6d8104bdca4d51e84b018
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-10 12:16:28 +0530

    Advanced DSA & Algorithms: MaxNetworkRank: in-progress v1

--- d_Graph/MaxNetworkRank.java
+++ d_Graph/MaxNetworkRank.java
@@ -3,5 +3,13 @@ public class MaxNetworkRank {
         int[] degree = new int[n];
         boolean[][] connected = new boolean[n][n];
 
+        for (int[] road : roads) {
+            int u = road[0], v = road[1];
+            degree[u]++;
+            degree[v]++;
+            connected[u][v] = true;
+            connected[v][u] = true;
+        }
+
     }
 }
