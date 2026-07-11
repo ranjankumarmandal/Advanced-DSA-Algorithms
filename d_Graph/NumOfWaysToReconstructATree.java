16b1e2685632be9fd8252705d4d0ff6787f6185e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-11 12:13:26 +0530

    Advanced DSA & Algorithms: NumOfWaysToReconstructATree: in-progress v1

--- d_Graph/NumOfWaysToReconstructATree.java
+++ d_Graph/NumOfWaysToReconstructATree.java
@@ -2,5 +2,12 @@ public class NumOfWaysToReconstructATree {
     public int checkWays(int[][] pairs) {
         Map<Integer, Set<Integer>> graph = new HashMap<>();
 
+        for (int[] p : pairs) {
+            graph.putIfAbsent(p[0], new HashSet<>());
+            graph.putIfAbsent(p[1], new HashSet<>());
+            graph.get(p[0]).add(p[1]);
+            graph.get(p[1]).add(p[0]);
+        }
+
     }
 }
