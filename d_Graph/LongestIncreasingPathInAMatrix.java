48e0e3f1767c741a9e3a6d757b49c05ca5f43a21
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-13 13:17:20 +0530

    Advanced DSA & Algorithms: LongestIncreasingPathInAMatrix: in-progress v1

--- d_Graph/LongestIncreasingPathInAMatrix.java
+++ d_Graph/LongestIncreasingPathInAMatrix.java
@@ -1,4 +1,8 @@
 public class LongestIncreasingPathInAMatrix {
     private static final int[][] DIRECTIONS = {
+            {0, 1},
+            {0, -1},
+            {1, 0},
+            {-1, 0}
     };
 }
