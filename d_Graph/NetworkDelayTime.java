98da491b7729b6230789009d4a011634463a6d18
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-14 15:31:57 +0530

    Advanced DSA & Algorithms: NetworkDelayNetwork: in-progress v2

--- d_Graph/NetworkDelayTime.java
+++ d_Graph/NetworkDelayTime.java
@@ -1,6 +1,9 @@
 public class NetowrkDelayTime {
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
-
+        for (int[] time : times) {
+            int u = time[0];
+            int v = time[1];
+        }
     }
 }
