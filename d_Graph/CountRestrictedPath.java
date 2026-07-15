de49eeb8b98a32c0af3bac67fe8137a4edd1f66e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-15 07:14:26 +0530

    Advanced DSA & Algorithms: CountRestrictedPath: in-progress v1

--- d_Graph/CountRestrictedPath.java
+++ d_Graph/CountRestrictedPath.java
@@ -1,5 +1,7 @@
 public class CountRestrictedPath {
     public int countRestrictedPaths(int n, int[][] edges) {
-
+        int rows=edges.length;
+        int cols=edges[0].length;
+        List<List<int[]>> adj=new ArrayList<>();
     }
 }
