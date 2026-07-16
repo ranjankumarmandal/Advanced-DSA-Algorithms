1079cc51326e3a8edacf88b857318350b035c88f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-16 06:47:48 +0530

    Advanced DSA & Algorithms: FindIfPathExistsInGraph: in-progress v2

--- d_Graph/FindIfPathExistsInGraph.java
+++ d_Graph/FindIfPathExistsInGraph.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class FindIfPathExistsInGraph {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
 
+        for (int[] e : edges) {
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
+        }
     }
 }
