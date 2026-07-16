77372e7da2cb8c2d451e53fd32729e71d45bcab0
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-16 06:51:29 +0530

    Advanced DSA & Algorithms: MinCostToReachDestinationInTime: in-progress v1

--- d_Graph/MinCostToReachDestinationInTime.java
+++ d_Graph/MinCostToReachDestinationInTime.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
 public class MinCostToReachDestinationInTime {
-
+    public int minCost(int maxTime, int[][] edges, int[] passingFees) {
+        int n = passingFees.length;
+    }
 }
