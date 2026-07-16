46cc58e54a3f148a4dd16d12251d8636c8eaf158
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-16 06:38:34 +0530

    Advanced DSA & Algorithms: CountWaysToBuildRoomsInAnAntColony: in-progressm v2

--- d_Graph/CountWaysToBuildRoomsInAnAntColony.java
+++ d_Graph/CountWaysToBuildRoomsInAnAntColony.java
@@ -4,5 +4,10 @@ public class CountWaysToBuildRoomsInAnAntColony {
     static final long MOD = 1000000007L;
 
     public int waysToBuildRooms(int[] prevRoom) {
+        int n = prevRoom.length;
+        List<Integer>[] g = new ArrayList[n];
+        for (int i = 0; i < n; i++) g[i] = new ArrayList<>();
+        for (int i = 1; i < n; i++) g[prevRoom[i]].add(i);
+
     }
 }
