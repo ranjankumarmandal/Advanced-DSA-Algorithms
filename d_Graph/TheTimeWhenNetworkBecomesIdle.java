ed960d25aebecd18a22dc02df7877e298f1888c2
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-17 18:02:20 +0530

    Advanced DSA & Algorithms: TheTimeWhenNetworkBecomesIdle: in-progress v2

--- d_Graph/TheTimeWhenNetworkBecomesIdle.java
+++ d_Graph/TheTimeWhenNetworkBecomesIdle.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class TheTimeWhenNetworkBecomesIdle {
     public int networkBecomesIdle(int[][] edges, int[] patience) {
+        int n = patience.length;
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
 
     }
 }
