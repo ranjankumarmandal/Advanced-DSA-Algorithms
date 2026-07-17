89e90dcb2bfde4b6feebff9b161e6681c28e9be6
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-17 18:09:04 +0530

    Advanced DSA & Algorithms: NumOfWaysToArriveAtDestination: in-progress v2

--- d_Graph/NumOfWaysToArriveAtDestination.java
+++ d_Graph/NumOfWaysToArriveAtDestination.java
@@ -4,6 +4,12 @@ public class NumOfWaysToArriveAtDestination {
     static final int MOD = 1_000_000_007;
 
     public int countPaths(int n, int[][] roads) {
+        List<long[]>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
 
+        for (int[] r : roads) {
+            graph[r[0]].add(new long[]{r[1], r[2]});
+            graph[r[1]].add(new long[]{r[0], r[2]});
+        }
     }
 }
