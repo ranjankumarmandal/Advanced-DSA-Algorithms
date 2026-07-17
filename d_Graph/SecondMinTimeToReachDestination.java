0563077403ddd34ad5d47024b4e88d55eeb98dcc
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-17 17:57:25 +0530

    Advanced DSA & Algorithms: SecondMinTimeToReachDestination: in-progress v2

--- d_Graph/SecondMinTimeToReachDestination.java
+++ d_Graph/SecondMinTimeToReachDestination.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class SecondMinTimeToReachDestination {
     public int secondMinimum(int n, int[][] edges, int time, int change) {
+        List<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
 
     }
 }
