c5eaca477c79b460a196279265c10fac3d642a56
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-18 19:36:04 +0530

    Advanced DSA & Algorithms: ParallelCourses3: in-progress v1

--- d_Graph/ParallelCourses3.java
+++ d_Graph/ParallelCourses3.java
@@ -1,5 +1,8 @@
 public class ParallelCourses3 {
     public int minimumTime(int n, int[][] relations, int[] time) {
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        int[] indegree = new int[n];
 
     }
 }
