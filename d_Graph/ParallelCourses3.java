1e56f4610e51fc0b7450fe4640f7455376c757ac
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-18 19:36:37 +0530

    Advanced DSA & Algorithms: ParallelCourses3: in-progress v3

--- d_Graph/ParallelCourses3.java
+++ d_Graph/ParallelCourses3.java
@@ -9,5 +9,13 @@ public class ParallelCourses3 {
             indegree[r[1] - 1]++;
         }
 
+        Queue<Integer> q = new ArrayDeque<>();
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = time[i];
+            if (indegree[i] == 0) q.offer(i);
+        }
+
     }
 }
