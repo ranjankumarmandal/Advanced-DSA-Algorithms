2e9ca520468048a683a2b527b867a762c72a8675
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-18 19:55:03 +0530

    Advanced DSA & Algorithms: MaxPathQualityOfAGraph: in-progress v3

--- d_Graph/MaxPathQualityOfAGraph.java
+++ d_Graph/MaxPathQualityOfAGraph.java
@@ -9,5 +9,14 @@ public class MaxPathQualityOfAGraph {
         int n = values.length;
         this.values = values;
         this.maxTime = maxTime;
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] e : edges) {
+            graph[e[0]].add(new int[]{e[1], e[2]});
+            graph[e[1]].add(new int[]{e[0], e[2]});
+        }
+        seen = new int[n];
+        dfs(0, 0, 0);
+        return ans;
     }
 }
