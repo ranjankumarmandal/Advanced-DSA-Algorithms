bde965a663fd7590861d13151a4c611d8da6bd61
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-18 20:01:01 +0530

    Advanced DSA & Algorithms: ProcessRequestedFriendRequest: in-progress v1

--- d_Graph/ProcessRequestedFriendRequest.java
+++ d_Graph/ProcessRequestedFriendRequest.java
@@ -1,5 +1,10 @@
 public class ProcessRequestedFriendRequest {
     class DSU {
         int[] parent, rank;
+
+        DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) parent[i] = i;
     }
 }
