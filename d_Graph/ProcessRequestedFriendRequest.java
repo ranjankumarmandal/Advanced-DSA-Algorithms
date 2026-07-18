916dd0c31950b941db84131291870a2b89982be2
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-18 20:03:04 +0530

    Advanced DSA & Algorithms: ProcessRequestedFriendRequest: in-progress v2

--- d_Graph/ProcessRequestedFriendRequest.java
+++ d_Graph/ProcessRequestedFriendRequest.java
@@ -6,5 +6,12 @@ public class ProcessRequestedFriendRequest {
             parent = new int[n];
             rank = new int[n];
             for (int i = 0; i < n; i++) parent[i] = i;
+        }
+
+        int find(int x) {
+            if (parent[x] != x) parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
     }
 }
