3404a9eb35032065ca7df3befa19558c4284b393
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-19 19:29:59 +0530

    Advanced DSA & Algorithms: ValidArrangementOfPairs: in-progress v2

--- d_Graph/ValidArrangementOfPairs.java
+++ d_Graph/ValidArrangementOfPairs.java
@@ -4,5 +4,11 @@ public class ValidArrangementOfPairs {
         Map<Integer, Integer> out = new HashMap<>();
         Map<Integer, Integer> in = new HashMap<>();
 
+        for (int[] p : pairs) {
+            graph.computeIfAbsent(p[0], k -> new ArrayDeque<>()).add(p[1]);
+            out.put(p[0], out.getOrDefault(p[0], 0) + 1);
+            in.put(p[1], in.getOrDefault(p[1], 0) + 1);
+        }
+
     }
 }
