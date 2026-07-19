c02d13a234591e519f81d5504d22f61dfb5d7eab
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-19 19:25:29 +0530

    Advanced DSA & Algorithms: FindAllPeopleWithSecret: in-progress v1

--- d_Graph/FindAllPeopleWithSecret.java
+++ d_Graph/FindAllPeopleWithSecret.java
@@ -2,5 +2,10 @@ public class FindAllPeopleWithSecret {
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
         Arrays.sort(meetings, (a, b) -> a[2] - b[2]);
 
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        union(parent, 0, firstPerson);
+
         }
 }
