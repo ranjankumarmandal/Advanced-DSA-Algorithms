6e7e6571b921d3816735440bad8a15c16161a7f0
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-19 19:34:45 +0530

    Advanced DSA & Algorithms: Graph: DetonateTheMaxBombs: in-progress v2

--- d_Graph/DetonateTheMaxBombs.java
+++ d_Graph/DetonateTheMaxBombs.java
@@ -4,5 +4,18 @@ public class DetonateTheMaxBombs {
         List<Integer>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
 
+        for (int i = 0; i < n; i++) {
+            long x1 = bombs[i][0], y1 = bombs[i][1], r = bombs[i][2];
+            long rr = r * r;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                long dx = x1 - bombs[j][0];
+                long dy = y1 - bombs[j][1];
+                if (dx * dx + dy * dy <= rr) {
+                    graph[i].add(j);
+                }
+            }
+        }
+
     }
 }
