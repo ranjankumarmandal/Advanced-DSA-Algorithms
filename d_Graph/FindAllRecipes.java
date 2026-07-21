569a2b91714dc48b267eef4b6a4a51a7d237fd0a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-21 22:08:14 +0530

    Advanced DSA & Algorithms: Graph: FindAllRecipes: in-progress v2

--- d_Graph/FindAllRecipes.java
+++ d_Graph/FindAllRecipes.java
@@ -7,5 +7,12 @@ public class FindAllRecipes {
             indegree.put(recipe, 0);
         }
 
+        for (int i = 0; i < recipes.length; i++) {
+            for (String ing : ingredients.get(i)) {
+                graph.computeIfAbsent(ing, k -> new ArrayList<>()).add(recipes[i]);
+                indegree.put(recipes[i], indegree.get(recipes[i]) + 1);
+            }
+        }
+
     }
 }
