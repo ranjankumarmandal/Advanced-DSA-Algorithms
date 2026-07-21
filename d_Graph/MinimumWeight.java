716b270ec7dd853092689b8eb613e902933659f3
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-21 22:31:32 +0530

    Advanced DSA & Algorithms: Graph: MinimumWeight: in-progress v1

--- d_Graph/MinimumWeight.java
+++ d_Graph/MinimumWeight.java
@@ -7,4 +7,26 @@ public class MinimumWeight {
             this.dist = dist;
         }
     }
+
+    private long[] dijkstra(List<List<Pair>> graph, int src) {
+        int n = graph.size();
+        long[] dist = new long[n];
+        Arrays.fill(dist, Long.MAX_VALUE);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Long.compare(a.dist, b.dist));
+        dist[src] = 0;
+        pq.offer(new Pair(src, 0));
+
+        while (!pq.isEmpty()) {
+            Pair cur = pq.poll();
+            if (cur.dist != dist[cur.node]) continue;
+
+            for (Pair next : graph.get(cur.node)) {
+                if (dist[next.node] > cur.dist + next.dist) {
+                    dist[next.node] = cur.dist + next.dist;
+                    pq.offer(new Pair(next.node, dist[next.node]));
+                }
+            }
+        }
+        return dist;
+    }
 }
