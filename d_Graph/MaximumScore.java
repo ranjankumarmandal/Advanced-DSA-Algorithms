5f276003ddc2cab998e68505d1f74be68a1805be
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-21 22:49:44 +0530

    Advanced DSA & Algorithms: Graph: MaximumScore: in-progress v2

--- d_Graph/MaximumScore.java
+++ d_Graph/MaximumScore.java
@@ -2,5 +2,7 @@ public class MaximumScore {
     public int maximumScore(int[] scores, int[][] edges) {
         int n = scores.length;
         List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+
     }
 }
