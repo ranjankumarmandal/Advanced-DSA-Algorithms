a6023f94e3f1fb165be4114a4e6d49e1ee090dc0
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-21 22:07:44 +0530

    Advanced DSA & Algorithms: Graph: FindAllRecipes: in-progress

--- d_Graph/FindAllRecipes.java
+++ d_Graph/FindAllRecipes.java
@@ -1,3 +1,5 @@
 public class FindAllRecipes {
-
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        Map<String, List<String>> graph = new HashMap<>();
+    }
 }
