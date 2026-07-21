2ca0a00be18181280f5ad0862b19163fc445272a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-21 22:27:38 +0530

    Advanced DSA & Algorithms: Graph: MinimumWeight: in-progress

--- d_Graph/MinimumWeight.java
+++ d_Graph/MinimumWeight.java
@@ -1,3 +1,10 @@
 public class MinimumWeight {
-
+    static class Pair {
+        int node;
+        long dist;
+        Pair(int node, long dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+    }
 }
