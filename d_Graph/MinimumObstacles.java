a40295d12ab3f0f6ec4744db3072607b2bb059cd
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-22 07:15:10 +0530

    Advanced DSA & Algorithms: Graph: MinimumObstacles: in-progress v2

--- d_Graph/MinimumObstacles.java
+++ d_Graph/MinimumObstacles.java
@@ -3,5 +3,8 @@ public class MinimumObstacles {
         int m = grid.length;
         int n = grid[0].length;
 
+        int[][] dist = new int[m][n];
+        for (int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
+
     }
 }
