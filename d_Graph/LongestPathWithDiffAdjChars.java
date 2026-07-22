f8ca836242a89e38b3eb04742c245d54ae83fa05
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-22 07:06:45 +0530

    Advanced DSA & Algorithms: Graph: LongestPathWithDiffAdjChars: in-progress v2

--- d_Graph/LongestPathWithDiffAdjChars.java
+++ d_Graph/LongestPathWithDiffAdjChars.java
@@ -2,5 +2,7 @@ public class LongestPathWithDiffAdjChars {
     private int ans = 1;
 
     private int dfs(int node, List<List<Integer>> tree, String s) {
+        int longest = 0;
+        int secondLongest = 0;
 
 }
