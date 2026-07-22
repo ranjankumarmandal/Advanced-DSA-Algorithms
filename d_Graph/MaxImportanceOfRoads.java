00aaaff75f0511dcc522e14233c5e25bfcd1434e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-22 07:11:23 +0530

    Advanced DSA & Algorithms: Graph: MaxImportanceOfRoads: in-progress v2

--- d_Graph/MaxImportanceOfRoads.java
+++ d_Graph/MaxImportanceOfRoads.java
@@ -2,5 +2,10 @@ public class MaxImportanceOfRoads {
     public long maximumImportance(int n, int[][] roads) {
         long[] degree = new long[n];
 
+        for (int[] road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+
     }
 }
