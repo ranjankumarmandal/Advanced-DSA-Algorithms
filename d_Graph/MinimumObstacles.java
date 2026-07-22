d7e66910cb4c657d33c60b0d36267321a60028a4
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-22 07:15:41 +0530

    Advanced DSA & Algorithms: Graph: MinimumObstacles: in-progress v4

--- d_Graph/MinimumObstacles.java
+++ d_Graph/MinimumObstacles.java
@@ -9,5 +9,13 @@ public class MinimumObstacles {
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
 
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offerFirst(new int[]{0, 0});
+        dist[0][0] = 0;
+
+        while (!dq.isEmpty()) {
+            int[] cur = dq.pollFirst();
+            int r = cur[0], c = cur[1];
+
     }
 }
