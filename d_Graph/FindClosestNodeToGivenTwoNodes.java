55e82f79a4a7bdff830395de78e197279a2a9254
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-23 07:20:51 +0530

    Advanced DSA & Algorithms: Graph: FindClosestNodeToGivenTwoNodes: in-progress v1

--- d_Graph/FindClosestNodeToGivenTwoNodes.java
+++ d_Graph/FindClosestNodeToGivenTwoNodes.java
@@ -1,5 +1,7 @@
 public class FindClosestNodeToGivenTwoNodes {
     public int closestMeetingNode(int[] edges, int node1, int node2) {
-
+        int n = edges.length;
+        int[] d1 = new int[n];
+        int[] d2 = new int[n];
     }
 }
