c622341840af3792f68724c6360a8adb80fb7f8c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-23 07:24:41 +0530

    Advanced DSA & Algorithms: Graph: CountPairs: in-progress v1

--- d_Graph/CountPairs.java
+++ d_Graph/CountPairs.java
@@ -1,4 +1,9 @@
 public class CountPairs {
     public long countPairs(int n, int[][] edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
     }
 }
