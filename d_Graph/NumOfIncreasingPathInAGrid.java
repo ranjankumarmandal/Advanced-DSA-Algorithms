e122196673bafe708e1d22914123644fd640bd34
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-23 07:15:03 +0530

    Advanced DSA & Algorithms: Graph: NumOfIncreasingPathInAGrid: in-progress v1

--- d_Graph/NumOfIncreasingPathInAGrid.java
+++ d_Graph/NumOfIncreasingPathInAGrid.java
@@ -2,4 +2,8 @@ public class NumOfIncreasingPathInAGrid {
     private static final int MOD = 1_000_000_007;
     private int[][] dp;
     private int[][] grid;
+    private int m, n;
+    private final int[] dr = {-1, 1, 0, 0};
+    private final int[] dc = {0, 0, -1, 1};
+
 }
