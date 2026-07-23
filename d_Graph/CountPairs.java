b8eab718bb1aa69a8830d99a1a496aa48630684b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-23 07:25:16 +0530

    Advanced DSA & Algorithms: Graph: CountPairs: in-progress v3

--- d_Graph/CountPairs.java
+++ d_Graph/CountPairs.java
@@ -10,5 +10,18 @@ public class CountPairs {
             graph.get(e[1]).add(e[0]);
         }
 
+        boolean[] vis = new boolean[n];
+        long ans = 0;
+        long remaining = n;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                long size = dfs(i, graph, vis);
+                remaining -= size;
+                ans += size * remaining;
+            }
+        }
+
+        return ans;
     }
 }
