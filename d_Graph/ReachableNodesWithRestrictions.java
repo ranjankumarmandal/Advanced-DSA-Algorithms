f690f8998e3302f289876d02a8ca6e0d01a2805a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-24 21:41:58 +0530

    Advanced DSA & Algorithms: Graph: ReachableNodesWithRestrictions: in-progress v2

--- d_Graph/ReachableNodesWithRestrictions.java
+++ d_Graph/ReachableNodesWithRestrictions.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class ReachableNodesWithRestrictions {
     public int reachableNodes(int n, int[][] edges, int[] restricted) {
+        Set<Integer> blocked = new HashSet<>();
+        for (int x : restricted) {
+            blocked.add(x);
+        }
 
     }
 }
