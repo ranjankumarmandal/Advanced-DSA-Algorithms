2241e5c1c29dcb9e6610c48f5e30e0ede5b8b273
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-24 21:37:45 +0530

    Advanced DSA & Algorithms: Graph: LongestCycleInAGraph: in-progress v3

--- d_Graph/LongestCycleInAGraph.java
+++ d_Graph/LongestCycleInAGraph.java
@@ -6,5 +6,15 @@ public class LongestCycleInAGraph {
         int ans = -1;
         int t = 1;
 
+        for (int i = 0; i < n; i++) {
+            if (state[i] != 0) continue;
+
+            int curr = i;
+            while (curr != -1 && state[curr] == 0) {
+                state[curr] = 1;
+                time[curr] = t++;
+                curr = edges[curr];
+            }
+        }
     }
 }
