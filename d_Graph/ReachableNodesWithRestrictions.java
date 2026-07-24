77d787e6d49bd06d6f865196ff5a2259f242d88d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-24 21:43:00 +0530

    Advanced DSA & Algorithms: Graph: ReachableNodesWithRestrictions: in-progress v4

--- d_Graph/ReachableNodesWithRestrictions.java
+++ d_Graph/ReachableNodesWithRestrictions.java
@@ -17,5 +17,12 @@ public class ReachableNodesWithRestrictions {
             graph[e[1]].add(e[0]);
         }
 
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+        visited[0] = true;
+
+        int count = 0;
+
     }
 }
