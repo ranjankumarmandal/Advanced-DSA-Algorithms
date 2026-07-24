ea0b401c509b844f5030ab341ca9de028a2f713d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-24 21:46:12 +0530

    Advanced DSA & Algorithms: Graph: NodeWithHighestEdgeScore: in-progress v2

--- d_Graph/NodeWithHighestEdgeScore.java
+++ d_Graph/NodeWithHighestEdgeScore.java
@@ -2,5 +2,9 @@ public class NodeWithHighestEdgeScore {
     public int edgeScore(int[] edges) {
         long[] score = new long[edges.length];
 
+        for (int i = 0; i < edges.length; i++) {
+            score[edges[i]] += i;
+        }
+
     }
 }
