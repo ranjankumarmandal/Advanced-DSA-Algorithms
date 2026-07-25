f09a6f24175b87f4461564cf4f34b02b720a415c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-25 15:24:35 +0530

    Advanced DSA & Algorithms: Graph: MostProfitablePathInATree: in-progress v2

--- d_Graph/MostProfitablePathInATree.java
+++ d_Graph/MostProfitablePathInATree.java
@@ -10,5 +10,13 @@ public class MostProfitablePathInATree {
         int n = amount.length;
         this.amount = amount;
 
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+
+        for (int[] e : edges) {
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
+        }
+
     }
 }
