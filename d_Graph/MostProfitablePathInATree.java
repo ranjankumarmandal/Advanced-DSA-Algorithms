51d682e21e47a49ea22bca2aa62cdc74fedca442
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-25 15:23:06 +0530

    Advanced DSA & Algorithms: Graph: MostProfitablePathInATree: in-progress

--- d_Graph/MostProfitablePathInATree.java
+++ d_Graph/MostProfitablePathInATree.java
@@ -1,3 +1,8 @@
 public class MostProfitablePathInATree {
-
+    List<Integer>[] graph;
+    int[] parent;
+    int[] depth;
+    int[] bobTime;
+    int[] amount;
+    int ans = Integer.MIN_VALUE;
 }
