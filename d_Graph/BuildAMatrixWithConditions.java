fc38e8fa2eb89c265d8d8c99d6b9dc76b71c0db1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-25 15:08:02 +0530

    Advanced DSA & Algorithms: Graph: BuildAMatrixWithConditions: in-progress v1

--- d_Graph/BuildAMatrixWithConditions.java
+++ d_Graph/BuildAMatrixWithConditions.java
@@ -1,5 +1,10 @@
 public class BuildAMatrixWithConditions {
     public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
+        int[] rowOrder = topoSort(k, rowConditions);
+        if (rowOrder == null) return new int[0][0];
+
+        int[] colOrder = topoSort(k, colConditions);
+        if (colOrder == null) return new int[0][0];
 
     }
 }
