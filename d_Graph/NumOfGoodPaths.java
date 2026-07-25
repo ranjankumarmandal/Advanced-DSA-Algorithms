52e4b15ab7b6eaf313eba1fba0d3fad660ca6c77
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-25 15:18:53 +0530

    Advanced DSA & Algorithms: Graph: NumOfGoodPaths: in-progress v1

--- d_Graph/NumOfGoodPaths.java
+++ d_Graph/NumOfGoodPaths.java
@@ -3,6 +3,13 @@ public class NumOfGoodPaths {
     int[] size;
 
     public int numberOfGoodPaths(int[] vals, int[][] edges) {
+        int n = vals.length;
 
+        List<int[]>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] e : edges) {
+            graph[e[0]].add(new int[]{e[1], Math.max(vals[e[0]], vals[e[1]])});
+            graph[e[1]].add(new int[]{e[0], Math.max(vals[e[0]], vals[e[1]])});
+        }
     }
 }
