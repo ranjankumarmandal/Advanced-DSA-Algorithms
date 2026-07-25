826d39f84387be1ffc009baad2d2b7518d8aaef1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-25 15:19:07 +0530

    Advanced DSA & Algorithms: Graph: NumOfGoodPaths: in-progress v2

--- d_Graph/NumOfGoodPaths.java
+++ d_Graph/NumOfGoodPaths.java
@@ -11,5 +11,11 @@ public class NumOfGoodPaths {
             graph[e[0]].add(new int[]{e[1], Math.max(vals[e[0]], vals[e[1]])});
             graph[e[1]].add(new int[]{e[0], Math.max(vals[e[0]], vals[e[1]])});
         }
+
+        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(vals[i], k -> new ArrayList<>()).add(i);
+        }
+
     }
 }
