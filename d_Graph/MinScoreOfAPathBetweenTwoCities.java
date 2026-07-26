c7235acc9723596061d372dcaa0514fe53cc348b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-26 20:31:44 +0530

    Advanced DSA & Algorithms: Graph: MinScoreOfAPathBetweenTwoCities: in-progres v1

--- d_Graph/MinScoreOfAPathBetweenTwoCities.java
+++ d_Graph/MinScoreOfAPathBetweenTwoCities.java
@@ -1,5 +1,6 @@
 public class MinScoreOfAPathBetweenTwoCities {
     public int minScore(int n, int[][] roads) {
         List<int[]>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
     }
 }
