dfc2c645b2f5957a5ea181c7edc21dd16209ebee
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-26 19:51:48 +0530

    Advanced DSA & Algorithms: Graph: MinFuelCostToReportToTheCapital: in-progress v2

--- d_Graph/MinFuelCostToReportToTheCapital.java
+++ d_Graph/MinFuelCostToReportToTheCapital.java
@@ -5,5 +5,12 @@ public class MinFuelCostToReportToTheCapital {
         int n = roads.length + 1;
         List<Integer>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] e : roads) {
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
+        }
+        dfs(0, -1, graph, seats);
+
+        return ans;
     }
 }
