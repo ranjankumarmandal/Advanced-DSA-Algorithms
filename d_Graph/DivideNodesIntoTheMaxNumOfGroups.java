f14c33137247f115a3b059621495b90f0a30a236
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-26 20:35:00 +0530

    Advanced DSA & Algorithms: Graph: DivideNodesIntoTheMaxNumOfGroups: in-progress v1

--- d_Graph/DivideNodesIntoTheMaxNumOfGroups.java
+++ d_Graph/DivideNodesIntoTheMaxNumOfGroups.java
@@ -1,6 +1,7 @@
 public class DivideNodesIntoTheMaxNumOfGroups {
     public int magnificentSets(int n, int[][] edges) {
         List<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
 
     }
 }
