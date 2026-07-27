e981a12cd0d700f96b3997145b804e3fe7ab6872
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-27 12:30:19 +0530

    Advanced DSA & Algorithms: Graph: MinTimeToVisitACellInAGrid: in-progress v2

--- d_Graph/MinTimeToVisitACellInAGrid.java
+++ d_Graph/MinTimeToVisitACellInAGrid.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class MinTimeToVisitACellInAGrid {
     public int minimumTime(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (m == 1 && n == 1) {
+            return 0;
+        }
 
     }
 }
