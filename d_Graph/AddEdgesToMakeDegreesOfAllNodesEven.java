da1707e1f4de24f095204e32d1b0f339c117b7cc
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-27 12:23:35 +0530

    Advanced DSA & Algorithms: Graph: AddEdgesToMakeDegreesOfAllNodesEven: in-progress v1

--- d_Graph/AddEdgesToMakeDegreesOfAllNodesEven.java
+++ d_Graph/AddEdgesToMakeDegreesOfAllNodesEven.java
@@ -1,5 +1,10 @@
 public class AddEdgesToMakeDegreesOfAllNodesEven {
     public boolean isPossible(int n, List<List<Integer>> edges) {
+        List<Integer>[] graph = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
     }
 }
