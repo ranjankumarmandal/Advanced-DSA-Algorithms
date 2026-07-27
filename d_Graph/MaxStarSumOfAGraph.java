58b4db4d9fcb7a35b932dd54eda7218246b34e04
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-27 12:17:24 +0530

    Advanced DSA & Algorithms: Graph: MaxStarSumOfAGraph: in-progress v2

--- d_Graph/MaxStarSumOfAGraph.java
+++ d_Graph/MaxStarSumOfAGraph.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class MaxStarSumOfAGraph {
     public int maxStarSum(int[] vals, int[][] edges, int k) {
+        int n = vals.length;
+        List<Integer>[] graph = new ArrayList[n];
 
     }
 }
