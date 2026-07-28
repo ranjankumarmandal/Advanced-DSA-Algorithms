8ad85f342aa295c92d2a4d5e74f62f8d85a43003
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-28 17:18:57 +0530

    Advanced DSA & Algorithms: Graph: CollectCoinsInATree: in-progress v3

--- d_Graph/CollectCoinsInATree.java
+++ d_Graph/CollectCoinsInATree.java
@@ -5,5 +5,9 @@ public class CollectCoinsInATree {
         int n = coins.length;
         if (n == 1) return 0;
 
+        List<Integer>[] g = new ArrayList[n];
+        for (int i = 0; i < n; i++) g[i] = new ArrayList<>();
+        int[] deg = new int[n];
+
     }
 }
