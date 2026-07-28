ab03100c62785a822f0cfe6fbc9afdbee1e9f1b7
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-28 17:27:12 +0530

    Advanced DSA & Algorithms: Graph: DesignGraphWithShortestPathCalculator: in-progress v1

--- d_Graph/DesignGraphWithShortestPathCalculator.java
+++ d_Graph/DesignGraphWithShortestPathCalculator.java
@@ -1,5 +1,14 @@
 import java.util.*;
 
 public class DesignGraphWithShortestPathCalculator {
+    private int n;
+    private List<int[]>[] graph;
+    private static final int INF = 1000000000;
 
+    public Graph(int n, int[][] edges) {
+        this.n = n;
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] e : edges) graph[e[0]].add(new int[]{e[1], e[2]});
+    }
 }
