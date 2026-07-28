e884c602d69233e1e04581f4ab5544dc50e3195f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-28 17:22:57 +0530

    Advanced DSA & Algorithms: Graph: ShortestCycleInAGraph: in-progress v2

--- d_Graph/ShortestCycleInAGraph.java
+++ d_Graph/ShortestCycleInAGraph.java
@@ -2,6 +2,10 @@ public class ShortestCycleInAGraph {
     public int findShortestCycle(int n, int[][] edges) {
         List<Integer>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] e : edges) {
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
+        }
 
     }
 }
