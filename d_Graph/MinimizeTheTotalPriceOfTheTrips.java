0841a0f61bf94a120adf2ab83b46a2798bcf4b9e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-29 06:43:48 +0530

    Advanced DSA & Algorithms: Graph: MinimizeTheTotalPriceOfTheTrips: in-progress v3

--- d_Graph/MinimizeTheTotalPriceOfTheTrips.java
+++ d_Graph/MinimizeTheTotalPriceOfTheTrips.java
@@ -8,5 +8,9 @@ public class MinimizeTheTotalPriceOfTheTrips {
         this.price = price;
         g = new ArrayList[n];
         for (int i = 0; i < n; i++) g[i] = new ArrayList<>();
+        for (int[] e : edges) {
+            g[e[0]].add(e[1]);
+            g[e[1]].add(e[0]);
+        }
     }
 }
