4e8bf535e69cc27ce61f8a17dff03e14de803ffa
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-29 06:48:03 +0530

    Advanced DSA & Algorithms: Graph: MinCostOfAPathWithSpecialRoads: in-progress v3

--- d_Graph/MinCostOfAPathWithSpecialRoads.java
+++ d_Graph/MinCostOfAPathWithSpecialRoads.java
@@ -4,5 +4,12 @@ public class MinCostOfAPathWithSpecialRoads {
         long[] dist = new long[n];
         Arrays.fill(dist, Long.MAX_VALUE);
 
+        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
+
+        for (int i = 0; i < n; i++) {
+            dist[i] = manhattan(start[0], start[1], specialRoads[i][0], specialRoads[i][1]) + specialRoads[i][4];
+            pq.offer(new long[]{dist[i], i});
+        }
+
     }
 }
