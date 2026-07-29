a7f4d689f873eb4635ec2a17d281daeda70954b1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-29 06:51:37 +0530

    Advanced DSA & Algorithms: Graph: CountCompleteComponents: in-progress v2

--- d_Graph/CountCompleteComponents.java
+++ d_Graph/CountCompleteComponents.java
@@ -3,6 +3,12 @@ public class CountCompleteComponents {
     boolean[] vis;
 
     public int countCompleteComponents(int n, int[][] edges) {
+        g = new ArrayList[n];
+        for (int i = 0; i < n; i++) g[i] = new ArrayList<>();
+        for (int[] e : edges) {
+            g[e[0]].add(e[1]);
+            g[e[1]].add(e[0]);
+        }
 
     }
 }
