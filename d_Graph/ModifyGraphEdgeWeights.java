f2a50e7284330919216571b7d3576d1276ef9793
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-30 17:45:25 +0530

    Advanced DSA & Algorithms: Graph: ModifyGraphEdgeWeights: in-progress v2

--- d_Graph/ModifyGraphEdgeWeights.java
+++ d_Graph/ModifyGraphEdgeWeights.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class ModifyGraphEdgeWeights {
     public int[][] modifiedGraphEdges(int n, int[][] edges, int source, int destination, int target) {
+        long inf = (long) 4e18;
 
+        long d = dijkstra(n, edges, source, destination);
+        if (d < target) return new int[0][];
     }
 }
