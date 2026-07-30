25304bcb3b1725d0907f549633e2b31aa4e09cfd
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-30 18:09:32 +0530

    Advanced DSA & Algorithms: Graph: MinEdgeReversals: in-progress v2

--- d_Graph/MinEdgeReversals.java
+++ d_Graph/MinEdgeReversals.java
@@ -3,6 +3,13 @@ public class MinEdgeReversals {
     int[] ans;
 
     public int[] minEdgeReversals(int n, int[][] edges) {
+        g = new ArrayList[n];
+        for (int i = 0; i < n; i++) g[i] = new ArrayList<>();
+
+        for (int[] e : edges) {
+            g[e[0]].add(new int[]{e[1], 0});
+            g[e[1]].add(new int[]{e[0], 1});
+        }
 
     }
 }
