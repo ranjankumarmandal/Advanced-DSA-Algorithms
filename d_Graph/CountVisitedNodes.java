fa8a35af8bd5826d77452bc174a6fbe684855fe1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-30 18:12:33 +0530

    Advanced DSA & Algorithms: Graph: CountVisitedNodes: in-progress v2

--- d_Graph/CountVisitedNodes.java
+++ d_Graph/CountVisitedNodes.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class CountVisitedNodes {
     public int[] countVisitedNodes(List<Integer> edges) {
+        int n = edges.size();
+        int[] indeg = new int[n];
+
+        for (int v : edges) indeg[v]++;
 
     }
 }
