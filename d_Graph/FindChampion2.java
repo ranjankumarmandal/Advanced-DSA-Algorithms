f03c4ce0af38f9d8b033d80f32c43cbb60538f33
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-31 22:18:07 +0530

    Advanced DSA & Algorithms: Graph: FindChampion2: in-progress v2

--- d_Graph/FindChampion2.java
+++ d_Graph/FindChampion2.java
@@ -2,5 +2,9 @@ public class FindChampion2 {
     public int findChampion(int n, int[][] edges) {
         int[] indegree = new int[n];
 
+        for (int[] e : edges) {
+            indegree[e[1]]++;
+        }
+
     }
 }
