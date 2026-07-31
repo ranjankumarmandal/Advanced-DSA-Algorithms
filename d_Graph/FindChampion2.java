11946752ef1740b0ce514fde92fb50b671dbba49
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-31 22:18:42 +0530

    Advanced DSA & Algorithms: Graph: FindChampion2: in-progress v4

--- d_Graph/FindChampion2.java
+++ d_Graph/FindChampion2.java
@@ -8,5 +8,11 @@ public class FindChampion2 {
 
         int ans = -1;
 
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                if (ans != -1) return -1;
+                ans = i;
+            }
+        }
     }
 }
