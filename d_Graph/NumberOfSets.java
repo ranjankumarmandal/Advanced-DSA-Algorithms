d2c5ed65ef93e86a8615c9d6adbbc86157fe761c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-31 22:10:41 +0530

    Advanced DSA & Algorithms: Graph: NumberOfSets: in-progress v2

--- d_Graph/NumberOfSets.java
+++ d_Graph/NumberOfSets.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class NumberOfSets {
     public int numberOfSets(int n, int maxDistance, int[][] roads) {
+        int ans = 0;
+        int total = 1 << n;
+        long inf = (long) 1e15;
 
     }
 }
