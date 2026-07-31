547b9280b12456b52838abcf57fa216abf75feba
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-31 22:11:09 +0530

    Advanced DSA & Algorithms: Graph: NumberOfSets: in-progress v3

--- d_Graph/NumberOfSets.java
+++ d_Graph/NumberOfSets.java
@@ -6,5 +6,19 @@ public class NumberOfSets {
         int total = 1 << n;
         long inf = (long) 1e15;
 
+        for (int mask = 0; mask < total; mask++) {
+            long[][] dist = new long[n][n];
+            for (int i = 0; i < n; i++) {
+                Arrays.fill(dist[i], inf);
+                dist[i][i] = 0;
+            }
+
+            for (int[] r : roads) {
+                int u = r[0], v = r[1], w = r[2];
+                if (((mask >> u) & 1) == 0 && ((mask >> v) & 1) == 0) {
+                    dist[u][v] = Math.min(dist[u][v], w);
+                    dist[v][u] = Math.min(dist[v][u], w);
+                }
+            }
     }
 }
