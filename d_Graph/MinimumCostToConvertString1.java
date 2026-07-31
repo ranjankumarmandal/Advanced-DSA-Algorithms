344b130f9f43f3abd9581dac65c296aa757738d6
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-07-31 22:15:26 +0530

    Advanced DSA & Algorithms: Graph: MinimumCostToConvertString1: in-progress v3

--- d_Graph/MinimumCostToConvertString1.java
+++ d_Graph/MinimumCostToConvertString1.java
@@ -5,5 +5,10 @@ public class MinimumCost {
         long[][] dist = new long[26][26];
         long inf = (long) 1e18;
 
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(dist[i], inf);
+            dist[i][i] = 0;
+        }
+
     }
 }
