1b9bf2d4574c8aed3b557a1e0ae81e93c50fe5e5
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-08-01 18:16:09 +0530

    Advanced DSA & Algorithms: Graph: CountOfPairs1: in-progress v2

--- d_Graph/CountOfPairs1.java
+++ d_Graph/CountOfPairs1.java
@@ -1,5 +1,13 @@
 public class CountOfPairs1 {
     public int[] countOfPairs(int n, int x, int y) {
         int[] ans = new int[n];
+        for (int i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                int d = Math.abs(i - j);
+                d = Math.min(d, Math.abs(i - x) + 1 + Math.abs(y - j));
+                d = Math.min(d, Math.abs(i - y) + 1 + Math.abs(x - j));
+                ans[d - 1] += 2;
+            }
+        }
     }
 }
