02bbe2a979253194f761f0034036d745fa2f2f30
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-08-01 18:11:06 +0530

    Advanced DSA & Algorithms: Graph: MinCostToConvertString2: in-progress v1

--- d_Graph/MinCostToConvertString2.java
+++ d_Graph/MinCostToConvertString2.java
@@ -3,4 +3,10 @@ public class MinCostToConvertString2 {
         Node[] next = new Node[26];
         int id = -1;
     }
+
+    public long minimumCost(String source, String target, String[] original, String[] changed, int[] cost) {
+        int m = original.length;
+        Node root = new Node();
+        int id = 0;
+    }
 }
